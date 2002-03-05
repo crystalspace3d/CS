0dd81de1c05e83b23a147480673241967784506c
nobody <nobody@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2002-03-05 16:52:12 +0000

    This commit was manufactured by cvs2svn to create branch 'avendor'.

---
+++ CS/migrated/branches/avendor/plugins/engine/iso/isoengin.cpp
@@ -0,0 +1,368 @@
+/*
+    Copyright (C) 2001 by W.C.A. Wijngaards
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "isoengin.h"
+#include "isoworld.h"
+#include "isoview.h"
+#include "isospr.h"
+#include "isomesh.h"
+#include "isolight.h"
+#include "ivideo/graph2d.h"
+#include "ivideo/graph3d.h"
+#include "ivideo/txtmgr.h"
+#include "isys/evdefs.h"
+#include "isys/event.h"
+#include "isys/system.h"
+#include "isys/vfs.h"
+#include "iutil/cfgmgr.h"
+#include "csutil/util.h"
+#include "iutil/object.h"
+#include "igraphic/imageio.h"
+#include "imesh/object.h"
+
+
+IMPLEMENT_IBASE (csIsoEngine)
+  IMPLEMENTS_INTERFACE (iIsoEngine)
+  IMPLEMENTS_INTERFACE (iPlugIn)
+IMPLEMENT_IBASE_END
+
+IMPLEMENT_FACTORY (csIsoEngine)
+
+EXPORT_CLASS_TABLE (iso)
+  EXPORT_CLASS_DEP (csIsoEngine, "crystalspace.engine.iso",
+    "Crystal Space Isometric Engine",
+    "crystalspace.kernel., crystalspace.graphics3d., crystalspace.graphics2d.")
+EXPORT_CLASS_TABLE_END
+
+csIsoEngine::csIsoEngine (iBase *iParent)
+{
+  CONSTRUCT_IBASE (iParent);
+  system = NULL;
+  g2d = NULL;
+  g3d = NULL;
+  txtmgr = NULL;
+
+  world = NULL;
+}
+
+csIsoEngine::~csIsoEngine ()
+{
+  for(int i=0; i<materials.Length(); i++)
+    RemoveMaterial(i);
+  if(g3d) g3d->DecRef();
+}
+
+bool csIsoEngine::Initialize (iSystem* p)
+{
+  system = p;
+  // Tell system driver that we want to handle broadcast events
+  if (!system->CallOnEvents (this, CSMASK_Broadcast))
+    return false;
+  return true;
+}
+
+bool csIsoEngine::HandleEvent (iEvent& Event)
+{
+  // Handle some system-driver broadcasts
+  if (Event.Type == csevBroadcast)
+    switch (Event.Command.Code)
+    {
+      case cscmdSystemOpen:
+      {
+        // system is open we can get ptrs now
+        g3d = QUERY_PLUGIN_ID (system, CS_FUNCID_VIDEO, iGraphics3D);
+        if (!g3d)
+        {
+          system->Printf(MSG_INTERNAL_ERROR, "IsoEngine: could not get G3D.\n");
+          return false;
+        }
+        g2d = g3d->GetDriver2D ();
+        if (!g2d) 
+        {
+          system->Printf(MSG_INTERNAL_ERROR, "IsoEngine: could not get G2D.\n");
+          return false;
+        }
+        txtmgr = g3d->GetTextureManager();
+        if (!txtmgr) 
+        {
+          system->Printf(MSG_INTERNAL_ERROR, 
+            "IsoEngine: could not get TextureManager.\n");
+          return false;
+        }
+        return true;
+      }
+      case cscmdSystemClose:
+      {
+        // We must free all material and texture handles since after
+        // G3D->Close() they all become invalid, no matter whenever
+        // we did or didn't an IncRef on them.
+	for(int i=0; i<materials.Length(); i++)
+	  RemoveMaterial(i);
+        return true;
+      }
+      case cscmdContextResize:
+      {
+        return false;
+      }
+      case cscmdContextClose:
+      {
+        return false;
+      }
+    } /* endswitch */
+
+  return false;
+}
+
+iIsoWorld* csIsoEngine::CreateWorld()
+{
+  return new csIsoWorld(this);
+}
+
+iIsoView* csIsoEngine::CreateView(iIsoWorld *world)
+{
+  return new csIsoView(this, this, world);
+}
+
+iIsoSprite* csIsoEngine::CreateSprite()
+{
+  return new csIsoSprite(this);
+}
+
+iIsoMeshSprite* csIsoEngine::CreateMeshSprite()
+{
+  return new csIsoMeshSprite(this);
+}
+
+int csIsoEngine::GetBeginDrawFlags () const
+{
+  return CSDRAW_CLEARZBUFFER;
+}
+
+iIsoSprite* csIsoEngine::CreateFloorSprite(const csVector3& pos, float w, 
+      float h)
+{
+  iIsoSprite *spr = new csIsoSprite(this);
+  spr->AddVertex(csVector3(0,0,0), 0, 0);
+  spr->AddVertex(csVector3(0, 0, w), 1, 0);
+  spr->AddVertex(csVector3(h, 0, w), 1, 1);
+  spr->AddVertex(csVector3(h, 0, 0), 0, 1);
+  spr->SetPosition(pos);
+  return spr;
+}
+
+iIsoSprite* csIsoEngine::CreateFrontSprite(const csVector3& pos, float w, 
+      float h)
+{
+  iIsoSprite *spr = new csIsoSprite(this);
+  float hw = w * 0.25;
+  spr->AddVertex(csVector3(-hw, 0,-hw), 0, 0);
+  spr->AddVertex(csVector3(-hw, h, -hw), 1, 0);
+  spr->AddVertex(csVector3(+hw, h, +hw), 1, 1);
+  spr->AddVertex(csVector3(+hw, 0, +hw), 0, 1);
+  spr->SetPosition(pos);
+  return spr;
+}
+
+iIsoSprite* csIsoEngine::CreateZWallSprite(const csVector3& pos, float w,
+  float h)
+{
+  iIsoSprite *spr = new csIsoSprite(this);
+  spr->AddVertex(csVector3(0, 0, 0), 0, 0);
+  spr->AddVertex(csVector3(0, h, 0), 1, 0);
+  spr->AddVertex(csVector3(0, h, w), 1, 1);
+  spr->AddVertex(csVector3(0, 0, w), 0, 1);
+  spr->SetPosition(pos);
+  return spr;
+}
+
+iIsoSprite* csIsoEngine::CreateXWallSprite(const csVector3& pos, float w,
+  float h)
+{
+  iIsoSprite *spr = new csIsoSprite(this);
+  spr->AddVertex(csVector3(0, 0, 0), 0, 0);
+  spr->AddVertex(csVector3(0, h, 0), 1, 0);
+  spr->AddVertex(csVector3(w, h, 0), 1, 1);
+  spr->AddVertex(csVector3(w, 0, 0), 0, 1);
+  spr->SetPosition(pos);
+  return spr;
+}
+
+iIsoLight* csIsoEngine::CreateLight()
+{
+  return new csIsoLight(this);
+}
+
+iMaterialWrapper *csIsoEngine::CreateMaterialWrapper(iMaterial *material,
+      const char *name)
+{
+  iMaterialWrapper* wrap = QUERY_INTERFACE(materials.NewMaterial(material),
+    iMaterialWrapper);
+  iObject *object = QUERY_INTERFACE(wrap, iObject);
+  object->SetName(name);
+  object->DecRef();
+  return wrap;
+}
+
+iMaterialWrapper *csIsoEngine::CreateMaterialWrapper(iMaterialHandle *handle,
+	    const char *name)
+{
+  iMaterialWrapper* wrap = QUERY_INTERFACE(materials.NewMaterial(handle),
+    iMaterialWrapper);
+  iObject *object = QUERY_INTERFACE(wrap, iObject);
+  object->SetName(name);
+  object->DecRef();
+  //printf("name %s = %d \n", name, materials.FindByName(name)->GetIndex());
+  return wrap;
+}
+
+iMaterialWrapper *csIsoEngine::CreateMaterialWrapper(const char *vfsfilename,
+	          const char *materialname)
+{
+  iImageIO *imgloader = QUERY_PLUGIN(system, iImageIO);
+  if(imgloader==NULL)
+  {
+    system->Printf(MSG_INTERNAL_ERROR, "Could not get image loader plugin.\n");
+    system->Printf(MSG_INTERNAL_ERROR, "Failed to load file %s.\n", 
+      vfsfilename);
+    return NULL;
+  }
+  iVFS *VFS = QUERY_PLUGIN(system, iVFS);
+  if(VFS==NULL)
+  {
+    system->Printf(MSG_INTERNAL_ERROR, "Could not get VFS plugin.\n");
+    system->Printf(MSG_INTERNAL_ERROR, "Failed to load file %s.\n", 
+      vfsfilename);
+    return NULL;
+  }
+
+  iDataBuffer *buf = VFS->ReadFile (vfsfilename);
+  if(!buf) 
+  {
+    system->Printf(MSG_INTERNAL_ERROR, "Could not read vfs file %s\n", 
+      vfsfilename);
+    return NULL;
+  }
+  iImage *image = imgloader->Load(buf->GetUint8 (), buf->GetSize (),
+    txtmgr->GetTextureFormat ());
+  if(!image) 
+  {
+    system->Printf(MSG_INTERNAL_ERROR, 
+      "The imageloader could not load image %s\n", vfsfilename);
+    return NULL;
+  }
+  iTextureHandle *handle = txtmgr->RegisterTexture(image, CS_TEXTURE_2D |
+    CS_TEXTURE_3D);
+  if(!handle) 
+  {
+    system->Printf(MSG_INTERNAL_ERROR, 
+      "Texturemanager could not register texture %s\n", vfsfilename);
+    return NULL;
+  }
+  csIsoMaterial *material = new csIsoMaterial(handle);
+  iMaterialHandle *math = txtmgr->RegisterMaterial(material);
+  if(!math) 
+  {
+    system->Printf(MSG_INTERNAL_ERROR, 
+      "Texturemanager could not register material %s\n", materialname);
+    return NULL;
+  }
+
+  buf->DecRef();
+  imgloader->DecRef();
+  return CreateMaterialWrapper(math, materialname);
+}
+
+iMaterialWrapper *csIsoEngine::FindMaterial(const char *name)
+{
+  return QUERY_INTERFACE(materials.FindByName(name), iMaterialWrapper);
+}
+
+iMaterialWrapper *csIsoEngine::FindMaterial(int index)
+{
+  return QUERY_INTERFACE(materials.Get(index), iMaterialWrapper);
+}
+
+void csIsoEngine::RemoveMaterial(const char *name)
+{
+  csIsoMaterialWrapper *wrap = materials.FindByName(name);
+  if(!wrap) return;
+  int i;
+  for(i=0; i< materials.Length(); i++)
+  {
+    if(materials.Get(i) == wrap) break;
+  }
+  materials.RemoveIndex(i);
+  delete wrap;
+}
+
+void csIsoEngine::RemoveMaterial(int index)
+{
+  csIsoMaterialWrapper *wrap = materials.Get(index);
+  if(!wrap) return;
+  materials.RemoveIndex(index);
+  delete wrap;
+}
+
+int csIsoEngine::GetNumMaterials() const
+{
+  return materials.Length();
+}
+
+
+iMeshObjectFactory *csIsoEngine::CreateMeshFactory(const char* classId,
+    const char *name) 
+{
+  if(name && FindMeshFactory(name))
+    return FindMeshFactory(name);
+  iMeshObjectType *mesh_type = QUERY_PLUGIN_CLASS (system, classId, "MeshObj", 
+    iMeshObjectType);
+  if(!mesh_type) mesh_type = LOAD_PLUGIN( system, classId,  "MeshObj", 
+    iMeshObjectType);
+  if(!mesh_type) return NULL;
+  iMeshObjectFactory *mesh_fact = mesh_type->NewFactory();
+  if(!mesh_fact) return NULL;
+  AddMeshFactory(mesh_fact, name);
+  mesh_fact->DecRef();
+  return mesh_fact;
+}
+
+void csIsoEngine::AddMeshFactory(iMeshObjectFactory *fact, const char *name)
+{
+  if(name && FindMeshFactory(name))
+    return;
+  fact->IncRef();
+  meshfactories.Push(new csIsoObjWrapper(fact, name));
+}
+
+iMeshObjectFactory *csIsoEngine::FindMeshFactory(const char *name)
+{
+  return (iMeshObjectFactory *)meshfactories.FindContentByName(name);
+}
+
+void csIsoEngine::RemoveMeshFactory(const char *name)
+{
+  int idx = meshfactories.FindIndexByName(name);
+  if(idx==-1) return;
+  csIsoObjWrapper *wrap = (csIsoObjWrapper*) meshfactories.Get(idx);
+  meshfactories.Delete(idx);
+  if(wrap->GetContent())
+    wrap->GetContent()->DecRef();
+  delete wrap;
+}
+
---
+++ CS/migrated/branches/avendor/plugins/engine/iso/isomesh.cpp
@@ -0,0 +1,426 @@
+/*
+    Copyright (C) 2001 by W.C.A. Wijngaards
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#define SYSDEF_ALLOCA
+#include "cssysdef.h"
+#include "isomesh.h"
+#include "ivideo/graph3d.h"
+#include "csgeom/math2d.h"
+#include "csgeom/polyclip.h"
+#include "ivideo/material.h"
+#include "ivideo/texture.h"
+#include "ivideo/txtmgr.h"
+#include "iengine/material.h"
+#include "imesh/object.h"
+#include "isys/system.h"
+
+#include "iengine/movable.h"
+#include "iengine/rview.h"
+#include "iengine/camera.h"
+
+//------------- Fake 3d Interfaces -----------------------------
+/// fake a movable for a MeshSprite
+class csIsoFakeMovable : public iMovable
+{
+  iIsoMeshSprite *isomesh;
+  /// a transform, for passing refs, information is kept in the mesh though.
+  csReversibleTransform obj;
+  long updatenumber;
+public:
+  DECLARE_IBASE;
+  csIsoFakeMovable(iIsoMeshSprite *t) {isomesh = t; updatenumber = 0;}
+  virtual ~csIsoFakeMovable() {}
+
+  //----- iMovable -------------------------------
+  virtual iMovable* GetParent () const {return 0;}
+  virtual void SetSector (iSector* ) { updatenumber++; }
+  virtual void ClearSectors () { updatenumber++; }
+  virtual void AddSector (iSector* ) { updatenumber++; }
+  virtual const csVector& GetSectors () const {return *(csVector*)0;}
+  virtual iSector* GetSector (int ) const {return 0;}
+  virtual int GetSectorCount () const { return 0; }
+  virtual bool InSector () const {return true;}
+  virtual void SetPosition (iSector*, const csVector3& v)
+  { isomesh->SetPosition(v); updatenumber++; }
+  virtual void SetPosition (const csVector3& v)
+  {
+    isomesh->SetPosition(v);
+    updatenumber++;
+  }
+  virtual const csVector3& GetPosition () const {return isomesh->GetPosition();}
+  virtual const csVector3 GetFullPosition () const {return isomesh->GetPosition();}
+  virtual void SetTransform (const csReversibleTransform& t) 
+  {
+    isomesh->SetTransform(t.GetT2O());
+    isomesh->SetPosition(t.GetOrigin());
+    updatenumber++;
+  }
+  virtual csReversibleTransform& GetTransform () 
+  { 
+    obj.SetT2O(isomesh->GetTransform()); 
+    obj.SetOrigin(isomesh->GetPosition());
+    return obj;
+  }
+  virtual csReversibleTransform GetFullTransform () const
+  { 
+    csReversibleTransform obj;
+    obj.SetT2O(isomesh->GetTransform()); 
+    obj.SetOrigin(isomesh->GetPosition());
+    return obj;
+  }
+  virtual void MovePosition (const csVector3& v)
+  {
+    isomesh->MovePosition(v);
+    updatenumber++;
+  }
+  virtual void SetTransform (const csMatrix3& matrix)
+  {
+    isomesh->SetTransform( matrix );
+    updatenumber++;
+  }
+  virtual void Transform (const csMatrix3& matrix)
+  {
+    isomesh->SetTransform( matrix * isomesh->GetTransform() );
+    updatenumber++;
+  }
+  virtual void AddListener (iMovableListener* /*listener*/, void* /*userdata*/)
+  {
+     /// does not work
+  }
+  virtual void RemoveListener (iMovableListener* )
+  {
+     /// does not work
+  }
+  virtual void UpdateMove () { updatenumber++; }
+  virtual long GetUpdateNumber () const
+  {
+     return updatenumber;
+  }
+
+};
+
+IMPLEMENT_IBASE (csIsoFakeMovable)
+  IMPLEMENTS_INTERFACE (iMovable)
+IMPLEMENT_IBASE_END
+
+
+/// fake 3d render view ...
+class csIsoFakeRenderView : public iRenderView {
+  iCamera *fakecam;
+  iIsoRenderView *isorview;
+  /**
+   * A callback function. If this is set then no drawing is done.
+   * Instead the callback function is called.
+   */
+  csDrawFunc* callback;
+  /// Userdata belonging to the callback.
+  void* callback_data;
+  
+public:
+  DECLARE_IBASE;
+  csIsoFakeRenderView() {}
+  virtual ~csIsoFakeRenderView() {}
+
+  /// set data to render an isometric mesh
+  void SetIsoData(iIsoRenderView *r, iCamera *cam)
+  {
+    isorview = r;
+    fakecam = cam;
+  }
+
+  //---------------- iRenderView -----------------------
+  virtual csRenderContext* GetRenderContext () {return 0;}
+  virtual void CreateRenderContext() {}
+  virtual void RestoreRenderContext (csRenderContext* ) {}
+  virtual iCamera* CreateNewCamera() {return fakecam;} //@@@ copy?
+  virtual iEngine* GetEngine () {return 0;}
+  virtual iGraphics2D* GetGraphics2D () 
+  {return isorview->GetG3D()->GetDriver2D();}
+  virtual iGraphics3D* GetGraphics3D () {return isorview->GetG3D();}
+  virtual void SetFrustum (float, float, float, float) {}
+  virtual void GetFrustum (float&, float&, float&, float&) {}
+  virtual iClipper2D* GetClipper () {return isorview->GetClipper();}
+  virtual void SetClipper (iClipper2D*) {}
+  virtual bool IsClipperRequired () {return false;}
+  virtual bool GetClipPlane (csPlane3& ) {return false;}
+  virtual csPlane3& GetClipPlane () {return *(csPlane3*)0;}
+  virtual void SetClipPlane (const csPlane3& ) {}
+  virtual void UseClipPlane (bool ) {}
+  virtual void UseClipFrustum (bool ) {}
+  virtual csFogInfo* GetFirstFogInfo () {return 0;}
+  virtual void SetFirstFogInfo (csFogInfo* ) {}
+  virtual bool AddedFogInfo () {return false;}
+  virtual void ResetFogInfo () {}
+  virtual iCamera* GetCamera () {return fakecam;}
+  virtual void CalculateFogPolygon (G3DPolygonDP& ) {}
+  virtual void CalculateFogPolygon (G3DPolygonDPFX& ) {}
+  virtual void CalculateFogMesh (const csTransform& , G3DTriangleMesh& ) {}
+  virtual bool ClipBBox (const csBox2& /*sbox*/, const csBox3& /*cbox*/,
+          int& clip_portal, int& clip_plane, int& clip_z_plane) 
+  {
+    // could clip more efficiently
+    // @@@ WARNING! This is potentially unsafe. A mesh that must be clipped
+    // MUST be clipped!
+    clip_portal = false;
+    clip_plane = false;
+    clip_z_plane = false;
+    return true;
+  }
+
+  virtual iSector* GetThisSector () {return 0;}
+  virtual void SetThisSector (iSector* ) {}
+  virtual iSector* GetPreviousSector () {return 0;}
+  virtual void SetPreviousSector (iSector* ) {}
+  virtual iPolygon3D* GetPortalPolygon () {return 0;}
+  virtual void SetPortalPolygon (iPolygon3D* ) {}
+  virtual int GetRenderRecursionLevel () {return 0;}
+  virtual void SetRenderRecursionLevel (int ) {}
+  virtual void AttachRenderContextData (void* key, iBase* data)
+  {
+    (void)key;
+    (void)data;
+  }
+  virtual iBase* FindRenderContextData (void* key)
+  {
+    (void)key;
+    return 0;
+  }
+  virtual void DeleteRenderContextData (void* key)
+  {
+    (void)key;
+  }
+  virtual void SetCallback (csDrawFunc* cb, void* cbdata)
+  {
+    callback = cb;
+    callback_data = cbdata;
+  }
+  virtual csDrawFunc* GetCallback ()
+  {
+    return callback;
+  }
+  virtual void* GetCallbackData ()
+  {
+    return callback_data;
+  }
+  virtual void CallCallback (int type, void* data)
+  {
+    callback (this, type, data);
+  }
+};
+
+IMPLEMENT_IBASE (csIsoFakeRenderView)
+  IMPLEMENTS_INTERFACE (iRenderView)
+IMPLEMENT_IBASE_END
+
+
+//------------ IsoMeshSprite ------------------------------------
+
+IMPLEMENT_IBASE (csIsoMeshSprite)
+  IMPLEMENTS_INTERFACE (iIsoMeshSprite)
+  IMPLEMENTS_INTERFACE (iIsoSprite)
+IMPLEMENT_IBASE_END
+
+csIsoMeshSprite::csIsoMeshSprite (iBase *iParent)
+{
+  CONSTRUCT_IBASE (iParent);
+  position.Set(0,0,0);
+  transform.Identity();
+  grid = NULL;
+  gridcall = NULL;
+  gridcalldata = NULL;
+  mesh = NULL;
+  zbufmode = CS_ZBUF_USE;
+}
+
+csIsoMeshSprite::~csIsoMeshSprite ()
+{
+  if(mesh) mesh->DecRef();
+}
+
+
+void csIsoMeshSprite::SetMeshObject(iMeshObject *mesh)
+{
+  if(mesh) mesh->IncRef();
+  if(csIsoMeshSprite::mesh) csIsoMeshSprite::mesh->DecRef();
+  csIsoMeshSprite::mesh = mesh;
+}
+
+void csIsoMeshSprite::Draw(iIsoRenderView *rview)
+{
+  //printf("isomeshsprite::Draw(%g, %g, %g)\n", position.x, position.y,
+    //position.z);
+
+  /// update animation
+  mesh->NextFrame(rview->GetView()->GetEngine()->GetSystem()->GetTime());
+
+  //iGraphics3D* g3d = rview->GetG3D ();
+  iIsoView* view = rview->GetView ();
+
+  // Prepare for rendering.
+  //g3d->SetRenderState (G3DRENDERSTATE_ZBUFFERMODE, zbufmode);
+
+  /// create a fake environment for the mesh so that it will render nicely.
+  /// the meshes use a perspective transform, and we do not want that.
+  /// but an approximation of the isometric transform can be given.
+
+  // It works like this:
+  // camera.x,y,z are a linear combination of x,y,z.
+  // -> this gives the fake camera transformation matrix.
+  // the fake camera z = iso.z-iso.x.
+  // The camera translation is then used to correct stuff.
+  // the z translation is set to -zlowerbound.
+  // Now the fov is set to iso.z - iso.x - zlowerbound.
+  // (for the iso coords of the center position).
+  // (this is why it is only an approximation).
+  //
+  // when the mesh object draws, the following will happen:
+  // the point is transformed using the given camera.
+  // iz = fov/z.   (note that fov is almost equal to z, so iz is almost 1)
+  // sx = x*iz + shiftx;
+  // sy = y*iz + shifty;
+  //   this will leave x,y almost unchanged, and shifted (as per iso camera).
+  // the depth buffer is filled with 1./z. , but this is (due to fake camera)
+  // equal to 1./(iso.z - iso.x - zlowerbound), which is exactly what the
+  // isometric engine uses as depth buffer values.
+
+  /// create fake moveable
+  csIsoFakeMovable* movable = new csIsoFakeMovable(this);
+
+  /// create fake camera
+  iCamera *fakecam = view->GetFakeCamera(position, rview);
+
+  /// create fake renderview
+  csIsoFakeRenderView *fakerview = new csIsoFakeRenderView();
+  fakerview->SetIsoData(rview, fakecam);
+
+  if(mesh->DrawTest(fakerview, movable))
+  {
+    //printf("mesh draw()\n");
+    /// UpdateLighting ....
+    iLight **lights = NULL;
+    int numlights = 0;
+    grid->GetFakeLights(position, lights, numlights);
+    mesh->UpdateLighting(lights, numlights, movable);
+    if(mesh->Draw(fakerview, movable, zbufmode))
+    {
+      //printf("mesh prob vis\n");
+      /// was probably visible
+    }
+  }
+
+  delete movable;
+  delete fakerview;
+  return;// true;
+}
+
+
+//------ further iIsoSprite compliance implementation ---------
+
+int csIsoMeshSprite::GetNumVertices() const
+{
+  return 0;
+}
+
+void csIsoMeshSprite::AddVertex(const csVector3& /*coord*/, float /*u*/, 
+  float /*v*/)
+{
+  /// no effect
+}
+
+
+void csIsoMeshSprite::SetPosition(const csVector3& newpos) 
+{
+  /// manage movement of the sprite, oldpos, newpos
+  csVector3 oldpos = position;
+  position = newpos; // already set, so it can be overridden
+  if(grid) grid->MoveSprite(this, oldpos, newpos);
+}
+
+void csIsoMeshSprite::MovePosition(const csVector3& delta) 
+{
+  SetPosition(position + delta);
+}
+
+
+void csIsoMeshSprite::SetMaterialWrapper(iMaterialWrapper * /*material*/)
+{
+  // nothing
+}
+
+iMaterialWrapper* csIsoMeshSprite::GetMaterialWrapper() const
+{
+  return NULL;
+}
+
+void csIsoMeshSprite::SetMixMode(UInt /*mode*/)
+{
+  // nothing
+}
+
+UInt csIsoMeshSprite::GetMixMode() const
+{
+  /// CS_FX_COPY -> rendered in main pass,
+  /// CS_FX_ADD -> in alpha pass
+  if((zbufmode == CS_ZBUF_USE)
+     ||(zbufmode == CS_ZBUF_FILL))
+     return CS_FX_COPY;
+  return CS_FX_ADD;
+}
+
+void csIsoMeshSprite::SetGrid(iIsoGrid *grid)
+{
+  if(csIsoMeshSprite::grid != grid)
+  {
+    csIsoMeshSprite::grid = grid;
+    if(gridcall) gridcall(this, gridcalldata);
+  }
+}
+
+void csIsoMeshSprite::SetAllColors(const csColor& /*color*/)
+{
+  // nothing to happen
+}
+
+const csVector3& csIsoMeshSprite::GetVertexPosition(int /*i*/)
+{
+  static csVector3 pos(0,0,0);
+  return pos;
+}
+
+void csIsoMeshSprite::AddToVertexColor(int /*i*/, const csColor& /*color*/)
+{
+  /// nothing
+}
+
+void csIsoMeshSprite::ResetAllColors()
+{
+  /// nothing
+}
+
+void csIsoMeshSprite::SetAllStaticColors(const csColor& /*color*/)
+{
+  /// nothing
+}
+
+void csIsoMeshSprite::AddToVertexStaticColor(int /*i*/, 
+  const csColor& /*color*/)
+{
+  /// nothing
+}
+
+

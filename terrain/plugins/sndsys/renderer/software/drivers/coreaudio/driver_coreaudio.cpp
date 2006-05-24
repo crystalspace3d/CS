30921000523b04e0ea374d940f097a88ebadcdb0
thebolt00 <thebolt00@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2006-05-24 21:07:31 +0000

    Created branch for improved terrain SoC project

---
+++ terrain/plugins/sndsys/renderer/software/drivers/coreaudio/driver_coreaudio.cpp
@@ -0,0 +1,329 @@
+/*
+    Copyright (C) 2005 by Andrew Mann
+    Based in part on work by Matt Reda
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+
+#include "csutil/event.h"
+#include "csutil/sysfunc.h"
+#include "iutil/cfgfile.h"
+#include "iutil/cmdline.h"
+#include "iutil/comp.h"
+#include "iutil/event.h"
+#include "iutil/eventh.h"
+#include "iutil/eventh.h"
+#include "iutil/eventq.h"
+#include "iutil/objreg.h"
+#include "iutil/plugin.h"
+#include "iutil/virtclk.h"
+#include "ivaria/reporter.h"
+
+#include "../../renderer.h"
+#include "isndsys/ss_driver.h"
+#include "isndsys/ss_renderer.h"
+#include "driver_coreaudio.h"
+
+CS_IMPLEMENT_PLUGIN
+
+CS_PLUGIN_NAMESPACE_BEGIN(SndSysCOREAUDIO)
+{
+
+SCF_IMPLEMENT_FACTORY (csSndSysDriverCoreAudio)
+
+SCF_IMPLEMENT_IBASE(csSndSysDriverCoreAudio)
+SCF_IMPLEMENTS_INTERFACE(iSndSysSoftwareDriver)
+SCF_IMPLEMENTS_EMBEDDED_INTERFACE(iComponent)
+SCF_IMPLEMENT_IBASE_END;
+
+SCF_IMPLEMENT_EMBEDDED_IBASE (csSndSysDriverCoreAudio::eiComponent)
+SCF_IMPLEMENTS_INTERFACE (iComponent)
+SCF_IMPLEMENT_EMBEDDED_IBASE_END
+
+// CoreAudio static IO procedure wrapper
+static OSStatus StaticAudioProc(AudioDeviceID inDevice,
+				const AudioTimeStamp *inNow,
+				const AudioBufferList *inInputData,
+				const AudioTimeStamp *inInputTime,
+				AudioBufferList *outOutputData,
+				const AudioTimeStamp *inOutputTime,
+				void *inClientData)
+{
+  csSndSysDriverCoreAudio *p_audio=(csSndSysDriverCoreAudio *)inClientData;
+  
+  return p_audio->AudioProc(inDevice, inNow, inInputData, inInputTime,
+			    outOutputData, inOutputTime);
+
+}
+
+csSndSysDriverCoreAudio::csSndSysDriverCoreAudio(iBase* piBase) :
+  object_reg(0), attached_renderer(0), running(false)
+{
+  SCF_CONSTRUCT_IBASE(piBase);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiComponent);
+  convert_buffer = 0;
+}
+
+csSndSysDriverCoreAudio::~csSndSysDriverCoreAudio()
+{
+  SCF_DESTRUCT_EMBEDDED_IBASE(scfiComponent);
+  SCF_DESTRUCT_IBASE();
+  free(convert_buffer);
+}
+
+void csSndSysDriverCoreAudio::Report(int severity, const char* msg, ...)
+{
+  va_list arg;
+  va_start (arg, msg);
+
+  csRef<iReporter> reporter = CS_QUERY_REGISTRY(object_reg, iReporter);
+
+  if (reporter)
+  {
+    reporter->ReportV (severity,
+      "crystalspace.sndsys.driver.software.coreaudio", msg, arg);
+  }
+  else
+  {
+    csPrintfV (msg, arg);
+    csPrintf ("\n");
+  }
+  va_end (arg);
+}
+
+bool csSndSysDriverCoreAudio::Initialize (iObjectRegistry* r)
+{
+  object_reg = r;
+  Report (CS_REPORTER_SEVERITY_DEBUG,
+    "Sound System: CoreAudio driver for software sound renderer initialized.");
+  return true;
+}
+
+bool csSndSysDriverCoreAudio::Open (csSndSysRendererSoftware *renderer,
+				   csSndSysSoundFormat *requested_format)
+{
+  uint32 propertysize, buffersize;
+  AudioStreamBasicDescription outStreamDesc;
+  //description of the data that CS will be providing
+  AudioStreamBasicDescription inStreamDesc;
+ 
+  OSStatus status;
+  
+  Report(CS_REPORTER_SEVERITY_DEBUG, "Sound System: CoreAudio Driver: Open()");
+
+  attached_renderer = renderer;
+
+  // Retrieve the output device ID - this is almost verbatim from the available
+  // sample code
+  propertysize = sizeof(outputDeviceID);
+  status = AudioHardwareGetProperty(kAudioHardwarePropertyDefaultOutputDevice,
+				    (UInt32*)&propertysize, &outputDeviceID);
+  if (status) 
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to obtain default CoreAudio output device.  Return of %d.",
+	   (int)status);
+    return false;
+  }
+  if (outputDeviceID == kAudioDeviceUnknown) 
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to obtain default CoreAudio output device.  "
+	   "Resulting ID is kAudioDeviceUnknown.");
+    return false;
+  }
+
+  // Set buffer size - 1/10th of a second buffer of floats
+  propertysize = sizeof(buffersize);
+  buffersize = requested_format->Freq * sizeof(float) *
+    requested_format->Channels / 10;
+	
+  convert_size = requested_format->Freq / 10;
+	
+  convert_buffer = malloc(convert_size * requested_format->Channels * requested_format->Bits/8);
+	
+  status = AudioDeviceSetProperty(outputDeviceID, 0, 0, false,
+    kAudioDevicePropertyBufferSize, propertysize, &buffersize);
+  if (status)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to set buffersize to %d bytes for CoreAudio output device. "
+	   "Return of %d.", buffersize, (int)status);
+	free(convert_buffer);
+    return false;
+  }
+  
+  // Get stream information
+  propertysize = sizeof(outStreamDesc);
+  status = AudioDeviceGetProperty(outputDeviceID, 0, false,
+    kAudioDevicePropertyStreamFormat, (UInt32*)&propertysize, &outStreamDesc);
+  if (status != 0)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to retrieve output stream description from CoreAudio "
+	   "output device.  Return of %d.", (int)status);
+    return false;
+  }
+  //get another copy to create the input format spec
+  status = AudioDeviceGetProperty(outputDeviceID, 0, false,
+    kAudioDevicePropertyStreamFormat, (UInt32*)&propertysize, &inStreamDesc);
+	   
+  // Set up source stream description for an AudioConverter to do its thang
+ 
+  inStreamDesc.mSampleRate = requested_format->Freq;
+  inStreamDesc.mFormatID = kAudioFormatLinearPCM;
+  inStreamDesc.mFormatFlags = kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked ;
+  if ((requested_format->Flags & CSSNDSYS_SAMPLE_ENDIAN_MASK) == CSSNDSYS_SAMPLE_BIG_ENDIAN)
+    inStreamDesc.mFormatFlags |= kAudioFormatFlagIsBigEndian;
+  inStreamDesc.mChannelsPerFrame = requested_format->Channels;
+  inStreamDesc.mBitsPerChannel = requested_format->Bits;
+  inStreamDesc.mFramesPerPacket = 1;
+  inStreamDesc.mBytesPerFrame = requested_format->Channels * requested_format->Bits / 8;
+  // The bytes per packet is purely a product of bytes per frame and frames per
+  // packet since we don't have any padding in a packet here
+  inStreamDesc.mBytesPerPacket =
+    inStreamDesc.mFramesPerPacket * inStreamDesc.mBytesPerFrame;
+/*
+   inStreamDesc.mSampleRate = 44100.00;
+  inStreamDesc.mFormatID = kAudioFormatLinearPCM;
+  inStreamDesc.mFormatFlags = kLinearPCMFormatFlagIsSignedInteger | kLinearPCMFormatFlagIsPacked ;
+  inStreamDesc.mChannelsPerFrame = 2;
+  inStreamDesc.mBitsPerChannel = 16;
+  inStreamDesc.mFramesPerPacket = 1;
+  inStreamDesc.mBytesPerFrame = 4;
+  // The bytes per packet is purely a product of bytes per frame and frames per
+  // packet since we don't have any padding in a packet here
+  inStreamDesc.mBytesPerPacket = 4;
+*/	
+	Report(CS_REPORTER_SEVERITY_WARNING,"Read in hardware properties of to Freq=%f Channels=%d Bits=%d FormatID=%s Flags=%x", outStreamDesc.mSampleRate,
+	   outStreamDesc.mChannelsPerFrame, outStreamDesc.mBitsPerChannel, &outStreamDesc.mFormatID, outStreamDesc.mFormatFlags );
+  // Set up destination stream parameters
+  outStreamDesc.mSampleRate = 44100.00;
+  outStreamDesc.mFormatID=kAudioFormatLinearPCM;
+  outStreamDesc.mFormatFlags = kAudioFormatFlagIsFloat | kAudioFormatFlagIsPacked | kAudioFormatFlagIsBigEndian;
+  outStreamDesc.mChannelsPerFrame= 2;
+  outStreamDesc.mBitsPerChannel= 32;
+//  outStreamDesc.mFramesPerPacket= 1;
+  // At this level, Core Audio demands 32 bit floats 
+  outStreamDesc.mBytesPerFrame = requested_format->Channels * sizeof(float);
+  // The bytes per packet is purely a product of bytes per frame and frames per
+  // packet since we don't have any padding in a packet here
+  outStreamDesc.mBytesPerPacket =
+    outStreamDesc.mFramesPerPacket * outStreamDesc.mBytesPerFrame;
+
+  //Make sure our HAL likes the output format?
+  propertysize = sizeof(outStreamDesc);
+  status = AudioDeviceSetProperty(outputDeviceID, 0, 0, false,
+    kAudioDevicePropertyStreamFormat, propertysize, &outStreamDesc);
+  if (status != 0)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to set output stream properties to  Freq=%f Channels=%d Bits=%d FormatID=%s Flags=%x. Return of %s", outStreamDesc.mSampleRate,
+	   outStreamDesc.mChannelsPerFrame, outStreamDesc.mBitsPerChannel, &outStreamDesc.mFormatID, outStreamDesc.mFormatFlags, &status);
+    //return false;
+  }
+  
+  //Create a converter to do the translation for us
+  status = AudioConverterNew(&inStreamDesc, &outStreamDesc, &converter);
+  if (status != 0)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to create converter with error %s", &status);
+    //return false;
+  }
+ 
+  // Copy the final format into local storage
+  memcpy(&playback_format, requested_format, sizeof(csSndSysSoundFormat));
+
+  // Add a callback and begin playback
+  status = AudioDeviceAddIOProc(outputDeviceID, StaticAudioProc, this);
+  if (status != 0)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR,
+	   "Failed to add audio device IO proc.  Return of %d.", (int)status);
+    return false;
+  }
+ 
+  
+  return true;
+}
+
+void csSndSysDriverCoreAudio::Close ()
+{
+  StopThread();
+  AudioDeviceRemoveIOProc(outputDeviceID, StaticAudioProc);
+  free(convert_buffer);
+  convert_buffer = 0;
+}
+
+bool csSndSysDriverCoreAudio::StartThread()
+{
+  OSStatus status;
+  // Since the Core Audio API is callback driven, we don't actually start a
+  // thread here ourselves.  Instead we start the audio device pulling from the
+  // audio procedure.  This runs in its own thread that we don't see.
+  Report(CS_REPORTER_SEVERITY_ERROR, "Getting our thread on!");
+  status = AudioDeviceStart(outputDeviceID, StaticAudioProc);
+  if (status != 0)
+  {
+    Report(CS_REPORTER_SEVERITY_ERROR, "Failed to start audio device IO proc. "
+	   "Return of %d.", (int)status);
+    return false;
+  }
+  running = true;
+  return true;
+}
+
+void csSndSysDriverCoreAudio::StopThread()
+{
+  running = false;
+  AudioDeviceStop(outputDeviceID, StaticAudioProc);
+  csSleep(100);
+}
+
+
+OSStatus csSndSysDriverCoreAudio::AudioProc(AudioDeviceID inDevice,
+					  const AudioTimeStamp *inNow,
+					  const AudioBufferList *inInputData,
+					  const AudioTimeStamp *inInputTime,
+					  AudioBufferList *outOutputData,
+					  const AudioTimeStamp *inOutputTime)
+{
+  if (!running)
+  {
+    outOutputData->mBuffers[0].mDataByteSize=0;
+    return 0;
+  }
+
+  // Fill the provided buffer with as many samples as possible and return the
+  // number of frames provided
+    int q = 0; 
+    OSStatus stat = 0;
+	int x = 0; //lame attempt to null-terminate OSStatus for string interpretation
+    size_t framesFilled = attached_renderer->FillDriverBuffer( convert_buffer, convert_size, 0, 0);
+	size_t bytesWritten = outOutputData->mBuffers[0].mDataByteSize;
+    stat = AudioConverterConvertBuffer(converter, framesFilled * playback_format.Bits/8 * playback_format.Channels, convert_buffer, &bytesWritten, outOutputData->mBuffers[0].mData); 
+    if(stat){
+	  //attached_renderer->RecordEvent(SSEC_DRIVER,SSEL_DEBUG, "Audio convert error %s",&stat);
+	}
+	//attached_renderer->RecordEvent(SSEC_DRIVER,SSEL_DEBUG, "CA requested %d bytes, we wrote %d.",outOutputData->mBuffers[0].mDataByteSize,bytesWritten);
+  return stat;
+}
+
+}
+CS_PLUGIN_NAMESPACE_END(SndSysCOREAUDIO)
+
---
+++ terrain/plugins/video/render3d/shader/shaderplugins/glshader_cg/glshader_cg.cpp
@@ -0,0 +1,305 @@
+/*
+Copyright (C) 2002 by Anders Stenberg
+                      Marten Svanfeldt
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Library General Public License for more details.
+
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free
+Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "csgeom/vector3.h"
+#include "csplugincommon/opengl/glextmanager.h"
+#include "csutil/objreg.h"
+#include "csutil/ref.h"
+#include "csutil/scf.h"
+#include "iutil/comp.h"
+#include "iutil/plugin.h"
+#include "ivaria/reporter.h"
+#include "ivideo/graph2d.h"
+#include "ivideo/graph3d.h"
+#include "ivideo/shader/shader.h"
+#include "glshader_cgvp.h"
+#include "glshader_cgfp.h"
+#include "glshader_cg.h"
+
+CS_IMPLEMENT_PLUGIN
+
+CS_PLUGIN_NAMESPACE_BEGIN(GLShaderCg)
+{
+
+CS_LEAKGUARD_IMPLEMENT (csGLShader_CG);
+
+SCF_IMPLEMENT_FACTORY (csGLShader_CG)
+
+csGLShader_CG::csGLShader_CG (iBase* parent) : 
+  scfImplementationType (this, parent)
+{
+  context = cgCreateContext ();
+  cgSetErrorHandler (ErrorHandler, 0);
+
+  enable = false;
+  isOpen = false;
+  debugDump = false;
+  dumpDir = 0;
+  ext = 0;
+}
+
+csGLShader_CG::~csGLShader_CG()
+{
+  delete[] dumpDir;
+  cgDestroyContext (context);
+}
+
+void csGLShader_CG::ErrorHandler (CGcontext context, CGerror error, 
+				  void* appData)
+{
+  iObjectRegistry* object_reg = (iObjectRegistry*)appData;
+  bool doVerbose;
+  csRef<iVerbosityManager> verbosemgr (
+    CS_QUERY_REGISTRY (object_reg, iVerbosityManager));
+  if (verbosemgr) 
+    doVerbose = verbosemgr->Enabled ("renderer.shader");
+  else
+    doVerbose = false;
+  if (!doVerbose) return;
+
+  csReport (object_reg, CS_REPORTER_SEVERITY_WARNING,
+    "crystalspace.graphics3d.shader.glcg",
+    "%s", cgGetErrorString (error));
+  if (error == CG_COMPILER_ERROR)
+  {
+    const char* listing = cgGetLastListing (context);
+    if (listing)
+    {
+      csReport (object_reg, CS_REPORTER_SEVERITY_WARNING,
+	"crystalspace.graphics3d.shader.glcg",
+	"%s", listing);
+    }
+  }
+}
+
+void csGLShader_CG::SplitArgsString (const char* str, ArgumentArray& args)
+{
+  if ((str == 0) || (*str == 0)) return;
+
+  csString s;
+  const char* p = str;
+  bool quote = false;
+  while (*p)
+  {
+    switch (*p)
+    {
+      case '"':
+        quote = !quote;
+        break;
+      case '\\':
+        if (quote)
+        {
+          p++;
+          s.Append (*p);
+          break;
+        }
+        // else fall through
+      case ' ':
+        if (!quote)
+        {
+          if (!s.IsEmpty()) args.Push (str);
+          args.Empty();
+        }
+        // else fall through
+      default:
+        s.Append (*p);
+    }
+    p++;
+  }
+  if (!s.IsEmpty()) args.Push (s);
+}
+
+void csGLShader_CG::GetProfileCompilerArgs (const char* type, 
+                                            CGprofile profile, 
+                                            ArgumentArray& args)
+{
+  csConfigAccess cfg (object_reg);
+  csString key ("Video.OpenGL.Shader.Cg.CompilerOptions");
+  SplitArgsString (cfg->GetStr (key), args);
+  key << "." << type;
+  SplitArgsString (cfg->GetStr (key), args);
+  key << "." << cgGetProfileString (profile);
+  SplitArgsString (cfg->GetStr (key), args);
+}
+
+////////////////////////////////////////////////////////////////////
+//                      iShaderProgramPlugin
+////////////////////////////////////////////////////////////////////
+bool csGLShader_CG::SupportType(const char* type)
+{
+  if (!Open())
+    return false;
+  if (!enable)
+    return false;
+  if( strcasecmp(type, "vp") == 0)
+    return true;
+  if( strcasecmp(type, "fp") == 0)
+    return true;
+  return false;
+}
+
+void csGLShader_CG::Report (int severity, const char* msg, ...)
+{
+  va_list arg;
+  va_start (arg, msg);
+  csReportV (object_reg, severity,
+    "crystalspace.graphics3d.shader.glcg", msg, arg);
+  va_end (arg);
+}
+
+csPtr<iShaderProgram> csGLShader_CG::CreateProgram(const char* type)
+{
+  if (!Open())
+    return 0;
+  if (strcasecmp(type, "vp") == 0)
+    return csPtr<iShaderProgram>(new csShaderGLCGVP(this));
+  else if (strcasecmp(type, "fp") == 0)
+    return csPtr<iShaderProgram>(new csShaderGLCGFP(this));
+  else
+    return 0;
+}
+
+bool csGLShader_CG::Open()
+{
+  if (isOpen) return true;
+  if (!object_reg) return false;
+
+  cgSetErrorHandler (ErrorHandler, object_reg);
+
+  csRef<iGraphics3D> r = CS_QUERY_REGISTRY(object_reg,iGraphics3D);
+
+  csRef<iFactory> f = SCF_QUERY_INTERFACE (r, iFactory);
+  if (f != 0 && strcmp ("crystalspace.graphics3d.opengl", 
+	f->QueryClassID ()) == 0)
+    enable = true;
+  else
+    return false;
+
+  r->GetDriver2D()->PerformExtension ("getextmanager", &ext);
+  if (ext == 0)
+  {
+    enable = false;
+    return false;
+  }
+
+  bool route = false;
+  csRef<iConfigManager> config(CS_QUERY_REGISTRY (object_reg, iConfigManager));
+  if (config->KeyExists ("Video.OpenGL.Shader.Cg.PSRouting"))
+  {
+    route = config->GetBool ("Video.OpenGL.Shader.Cg.PSRouting");
+    if (doVerbose)
+      Report (CS_REPORTER_SEVERITY_NOTIFY,
+        "Routing Cg fragment programs to Pixel Shader plugin %s (forced).", 
+        route ? "ON" : "OFF");
+  }
+  else
+  {
+    ext->InitGL_ARB_fragment_program ();
+    ext->InitGL_ATI_fragment_shader ();
+
+    // If the hardware's got ATI_f_p, but isn't new enough to have ARB_f_p
+    // we default to routing Cg fragment programs to the Pixel Shader plugin
+    route = !ext->CS_GL_ARB_fragment_program && 
+            ext->CS_GL_ATI_fragment_shader;
+    if (doVerbose)
+      Report (CS_REPORTER_SEVERITY_NOTIFY,
+        "Routing Cg fragment programs to Pixel Shader plugin %s (default).", 
+        route ? "ON" : "OFF");
+  }
+
+  debugDump = config->GetBool ("Video.OpenGL.Shader.Cg.DebugDump", false);
+  if (debugDump)
+    dumpDir = csStrNew (config->GetStr ("Video.OpenGL.Shader.Cg.DebugDumpDir",
+    "/tmp/cgdump/"));
+
+  if (route)
+  {
+    psplg = csLoadPluginCheck<iShaderProgramPlugin> (object_reg,
+      "crystalspace.graphics3d.shader.glps1", false);
+    if(!psplg)
+    {
+      if (doVerbose)
+        Report (CS_REPORTER_SEVERITY_WARNING,
+            "Could not find crystalspace.graphics3d.shader.glps1. Cg to PS "
+            "routing unavailable.");
+    }
+  }
+  // Check which FP profile to use...
+  if (psplg)
+  {
+    ext->InitGL_ATI_fragment_shader ();
+    ext->InitGL_NV_texture_shader ();
+    ext->InitGL_NV_texture_shader2 ();
+    if (ext->CS_GL_ATI_fragment_shader)
+    {
+      psProfile = CG_PROFILE_PS_1_3;
+    }
+    else if (ext->CS_GL_NV_texture_shader && ext->CS_GL_NV_texture_shader2)
+    {
+      // @@@ Is that logic correct?
+      ext->InitGL_NV_register_combiners2 ();
+      if (ext->CS_GL_NV_register_combiners2)
+      {
+        ext->InitGL_NV_texture_shader3 ();
+        if (ext->CS_GL_NV_texture_shader3)
+        {
+          psProfile = CG_PROFILE_PS_1_3;
+        }
+        else
+        {
+          psProfile = CG_PROFILE_PS_1_2;
+        }
+      }
+      else
+      {
+        psProfile = CG_PROFILE_PS_1_1;
+      }
+    }
+    else
+    {
+      if (doVerbose)
+        Report (CS_REPORTER_SEVERITY_WARNING,
+          "Cg to PS routing disabled due lack of hardware support.");
+      psplg = 0;
+    }
+  }
+
+  isOpen = true;
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////
+//                          iComponent
+////////////////////////////////////////////////////////////////////
+bool csGLShader_CG::Initialize(iObjectRegistry* reg)
+{
+  object_reg = reg;
+  csRef<iVerbosityManager> verbosemgr (
+    CS_QUERY_REGISTRY (object_reg, iVerbosityManager));
+  if (verbosemgr) 
+    doVerbose = verbosemgr->Enabled ("renderer.shader");
+  else
+    doVerbose = false;
+  return true;
+}
+
+}
+CS_PLUGIN_NAMESPACE_END(GLShaderCg)
+

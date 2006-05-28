e3e1766062712e99312a62f76178dd26ef8a98f9
thebolt00 <thebolt00@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2006-05-28 21:06:30 +0000

    Created branch feature/newparticles

---
+++ CS/branches/feature/newparticles/include/csutil/util.h
@@ -0,0 +1,217 @@
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_UTIL_H__
+#define __CS_UTIL_H__
+
+#include <stdio.h>
+#include "csextern.h"
+#include "csutil/csunicode.h"
+
+/**\file
+ * Miscellaneous utilities
+ */
+
+/**\addtogroup util
+ * @{ */
+ 
+/**
+ * Allocate a new char [] and copy the string into the newly allocated 
+ * storage.
+ * This is a handy method for copying strings, in fact it is the C++ analogue
+ * of the strdup() function from string.h (strdup() is not present on some
+ * platforms). To free the pointer the caller should call delete[].
+ */
+CS_CRYSTALSPACE_EXPORT char *csStrNew (const char *s);
+/**
+ * Allocate a new char [] and copy an UTF-8 version of the string into 
+ * the newly allocated storage.
+ */
+CS_CRYSTALSPACE_EXPORT char *csStrNew (const wchar_t *s);
+/**
+ * Allocate a new widechar [] and the string into the newly allocated storage.
+ */
+CS_CRYSTALSPACE_EXPORT wchar_t* csStrNewW (const wchar_t *s);
+/**
+ * Allocate a new widechar [] and copy the string converted from UTF-8 into 
+ * the newly allocated storage.
+ */
+CS_CRYSTALSPACE_EXPORT wchar_t* csStrNewW (const char *s);
+
+/**
+ * Perform case-insensitive string comparison. Returns a negative number if \p
+ * str1 is less than \p str2, zero if they are equal, or a positive number if
+ * \p str1 is greater than \p str2. For best portability, use function rather
+ * than strcasecmp() or stricmp().
+ */
+CS_CRYSTALSPACE_EXPORT int csStrCaseCmp(char const* str1, char const* str2);
+
+/**
+ * Perform case-insensitive string comparison of the first \p n characters of
+ * \p str1 and \p str2. Returns a negative number if the n-character prefix of
+ * \p str1 is less than \p str2, zero if they are equal, or a positive number
+ * if the prefix of \p str1 is greater than \p str2. For best portability, use
+ * function rather than strncasecmp() or strnicmp().
+ */
+CS_CRYSTALSPACE_EXPORT int csStrNCaseCmp(char const* str1, char const* str2,
+  size_t n);
+
+/**
+ * Helper class to convert wchar_t* to char* (UTF-8 encoded) strings for use
+ * as function parameters.
+ * Use of this helper class is more convenient than a csStrNew() / delete[]
+ * pair, but essentially does the same (with the convenience of automatic
+ * cleanup).
+ * \code
+ *   wchar_t* wstr = L"Hello World";
+ *    ... 
+ *   iNativeWindow* natwin =  ... ;
+ *     natwin->SetTitle (csWtoC (wstr));
+ * \endcode
+ */
+struct csWtoC
+{
+private:
+  char* s;
+public:
+  /**
+   * Constructor.
+   * Stores an UTF-8 converted version of \p ws internally.   
+   */
+  csWtoC (const wchar_t* ws)
+  { s = csStrNew (ws); }
+  /**
+   * Deletes the internally stored string.
+   */
+  ~csWtoC ()
+  { delete[] s; }
+  /**
+   * Retrieve the converted string.
+   */
+  operator const char* () const
+  { return s; }
+};
+
+/**
+ * Helper class to convert char* (UTF-8 encoded )to wchar_t* strings for use
+ * as function parameters.
+ * Use of this helper class is more convenient than a csStrNewW() / delete[]
+ * pair, but essentially does the same (with the convenience of automatic
+ * cleanup).
+ */
+struct csCtoW
+{
+private:
+  wchar_t* ws;
+public:
+  /**
+   * Constructor.
+   * Stores an UTF-16 converted version of \p s internally.   
+   */
+  csCtoW (const char* s)
+  { ws = csStrNewW (s); }
+  /**
+   * Deletes the internally stored string.
+   */
+  ~csCtoW ()
+  { delete[] ws; }
+  /**
+   * Retrieve the converted string.
+   */
+  operator const wchar_t* () const
+  { return ws; }
+};
+
+/**
+ * Expand a filename if it contains shortcuts.
+ * Currently the following macros are recognised and expanded:
+ * <pre>
+ * '.', '~', '..', 'drive:' (on DOS/Win32/OS2)
+ * </pre>
+ * The returned filename is always absolute, i.e. it always starts
+ * from root. Return a string allocated with csStrNew().
+ */
+CS_CRYSTALSPACE_EXPORT char *csExpandName (const char *iName);
+
+/**
+ * Split a pathname into separate path and name.
+ */
+CS_CRYSTALSPACE_EXPORT void csSplitPath (const char *iPathName, char *oPath,
+  size_t iPathSize, char *oName, size_t iNameSize);
+
+/**
+ * Perform shell-like filename \e globbing (pattern matching).
+ * The special token \p * matches zero or more characters, and the token \p ? 
+ * matches exactly one character. Examples: "*a*.txt", "*a?b*", "*"
+ * Character-classes \p [a-z] are not understood by this function.
+ * \remark If you want case-insensitive comparison, convert \p fName and
+ *   \p fMask to upper- or lower-case first.
+ */
+CS_CRYSTALSPACE_EXPORT bool csGlobMatches (const char *fName,
+	const char *fMask);
+
+/**
+ * Finds the smallest number that is a power of two and is larger or
+ * equal to n.
+ */
+CS_CRYSTALSPACE_EXPORT int csFindNearestPowerOf2 (int n);
+
+/// Returns true if n is a power of two.
+CS_CRYSTALSPACE_EXPORT bool csIsPowerOf2 (int n);
+
+/// Find the log2 of 32bit argument.
+static inline int csLog2 (int n)
+{
+  const unsigned int b[] = {0x2, 0xC, 0xF0, 0xFF00, 0xFFFF0000};
+  const unsigned int S[] = {1, 2, 4, 8, 16};
+  int i;
+
+  register unsigned int c = 0; // result of log2(v) will go here
+  for (i = 4; i >= 0; i--) // unroll for speed...
+  {
+    if (n & b[i])
+    {
+      n >>= S[i];
+      c |= S[i];
+    } 
+  }
+  return c;
+}
+
+/**
+ * Given \p src and \p dest, which are already allocated, copy \p source to \p
+ * dest.  But, do not copy \p search, instead replace that with \p replace
+ * string.  \p max is size in bytes of \p dest.
+ */
+CS_CRYSTALSPACE_EXPORT void csReplaceAll (char *dest, const char *src,
+  const char *search, const char *replace, int max);
+
+/**
+ * Given \p src and \p dest, which are already allocated, copy \p source to \p
+ * dest.  But, do not copy \p search, instead replace that with \p replace
+ * string.  \p max is size in bytes of \p dest.
+ * \deprecated Use csReplaceAll() instead.
+ */
+CS_DEPRECATED_METHOD 
+inline void csFindReplace (char *dest, const char *src,
+  const char *search, const char *replace, int max)
+{ csReplaceAll(dest, src, search, replace, max); }
+
+/** @} */
+  
+#endif // __CS_UTIL_H__
---
+++ CS/branches/feature/newparticles/plugins/video/render3d/software/soft_txt.cpp
@@ -0,0 +1,485 @@
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "soft_txt.h"
+#include "csgfx/bakekeycolor.h"
+#include "csgfx/imagemanipulate.h"
+#include "csgfx/memimage.h"
+#include "csgfx/packrgb.h"
+#include "csgfx/xorpat.h"
+#include "iutil/cfgfile.h"
+#include "iutil/event.h"
+#include "iutil/eventq.h"
+#include "igraphic/image.h"
+#include "ivaria/reporter.h"
+#include "csqint.h"
+#include "soft_g3d.h"
+
+CS_PLUGIN_NAMESPACE_BEGIN(Soft3D)
+{
+
+void csSoftwareTexture::compute_masks ()
+{
+  shf_w = csLog2 (w);
+  and_w = (1 << shf_w) - 1;
+  shf_h = csLog2 (h);
+  and_h = (1 << shf_h) - 1;
+}
+
+void csSoftwareTexture::ImageToBitmap (iImage *Image)
+{
+  csRef<iImage> img;
+  if (parent->GetKeyColor ())
+    img = csBakeKeyColor::Image (Image, parent->transp_color);
+  else
+    img = Image;
+
+  size_t pixNum = w * h;
+  bitmap = new uint32[pixNum];
+#ifdef CS_LITTLE_ENDIAN
+  if (csPackRGBA::IsRGBpixelSane() 
+    && !parent->texman->G3D->pixelBGR)
+  {
+    memcpy (bitmap, img->GetImageData(), pixNum * sizeof (uint32));
+  }
+  else
+#endif
+  if (parent->texman->G3D->pixelBGR)
+  {
+    csRGBpixel* p = (csRGBpixel*)img->GetImageData();
+    uint32* dst = bitmap;
+    while (pixNum-- > 0)
+    {
+      Pixel px (p->blue, p->green, p->red, p->alpha);
+      *dst = px.ui32;
+      dst++; p++;
+    }
+  }
+  else
+  {
+    csRGBpixel* p = (csRGBpixel*)img->GetImageData();
+    uint32* dst = bitmap;
+    while (pixNum-- > 0)
+    {
+      Pixel px (p->red, p->green, p->blue, p->alpha);
+      *dst = px.ui32;
+      dst++; p++;
+    }
+  }
+}
+
+//----------------------------------------------- csSoftwareTextureHandle ---//
+
+csSoftwareTextureHandle::csSoftwareTextureHandle (
+	csSoftwareTextureManager *texman, iImage *Image, int flags)
+	: csTextureHandle (texman, flags), image (Image)
+{
+  if (flags & CS_TEXTURE_3D)
+  {
+    int newwidth = 0, newheight = 0, newdepth = 0;
+    AdjustSizePo2 (image->GetWidth(), image->GetHeight(), image->GetDepth(),
+      newwidth, newheight, newdepth);
+    if (newwidth != image->GetWidth () || newheight != image->GetHeight ())
+      image = csImageManipulate::Rescale (image, newwidth, newheight, newdepth);
+  }
+  this->texman = texman;
+  prepared = false;
+  memset (tex, 0, sizeof (tex));
+
+  if (image.IsValid() && image->GetFormat () & CS_IMGFMT_ALPHA)
+    alphaType = csAlphaMode::alphaSmooth;
+  else if (image.IsValid() && image->HasKeyColor ())
+  {
+    alphaType = csAlphaMode::alphaBinary;
+    int r,g,b;
+    image->GetKeyColor (r,g,b);
+    SetKeyColor (r, g, b);
+  }
+  else
+    alphaType = csAlphaMode::alphaNone;
+}
+
+csSoftwareTextureHandle::~csSoftwareTextureHandle ()
+{
+  if (texman) texman->UnregisterTexture (this);
+}
+
+void csSoftwareTextureHandle::CreateMipmaps ()
+{
+  if (!image) return;
+
+  csRGBpixel *tc = transp ? &transp_color : (csRGBpixel *)0;
+
+  // Delete existing mipmaps, if any
+  int i;
+  for (i = 0; i < 4; i++)
+  {
+    delete tex [i];
+  }
+
+  tex [0] = new csSoftwareTexture (this, image);
+
+  // 2D textures uses just the top-level mipmap
+  if ((flags & (CS_TEXTURE_3D | CS_TEXTURE_NOMIPMAPS)) == CS_TEXTURE_3D)
+  {
+    csRef<iImage> thisImage = image;
+    int nMipmaps = thisImage->HasMipmaps();
+    
+    for (int i = 1; i < 4; i++)
+    {
+      csRef<iImage> newImage;
+      if (i <= nMipmaps)
+	newImage = image->GetMipmap (i);
+      if (!newImage.IsValid())
+      {
+	newImage = csImageManipulate::Mipmap (thisImage, 1, tc);
+	if (texman->sharpen_mipmaps)
+	  newImage = csImageManipulate::Sharpen (newImage, texman->sharpen_mipmaps, tc);
+      }
+      thisImage = newImage;
+
+      if (texman->debugMipmaps)
+      {
+	newImage.AttachNew (new csImageMemory (newImage));
+	size_t numPx = newImage->GetWidth() * newImage->GetHeight();
+	csRGBpixel* p = (csRGBpixel*)newImage->GetImageData();
+	while (numPx-- > 0)
+	{
+	  if (i >= 2) p->red = 0;
+	  if ((i == 1) || (i == 3)) p->blue = 0;
+	  if (i <= 2) p->green = 0;
+	  p++;
+	}
+      }
+
+      tex [i] = new csSoftwareTexture (this, newImage);
+    }
+  }
+}
+
+void csSoftwareTextureHandle::PrepareInt ()
+{
+  if (prepared) return;
+  prepared = true;
+  CreateMipmaps ();
+  image = 0;
+}
+
+void csSoftwareTextureHandle::Blit (int x, int y, int width, int height,
+				    unsigned char const* data, 
+				    TextureBlitDataFormat format)
+{
+  csSoftwareTexture *tex0 = (csSoftwareTexture *)tex[0];
+  uint32* bitmap = tex0->bitmap;
+  uint8 *src = (uint8*)data;
+
+  int tex_w, tex_h;
+  tex_w = tex0->w;
+  tex_h = tex0->h;
+
+  int blit_w = x+width;
+  int blit_h = y+height;
+
+  if (blit_w > tex_w) blit_w = tex_w;
+  if (blit_h > tex_h) blit_h = tex_h;
+
+  if (x > tex_w) return;
+  if (y > tex_h) return;
+
+  int rx, ry;
+  for (ry = y; ry < blit_h; ry++)
+  {
+    uint32* linestart = bitmap + (ry*tex_w + x);
+#ifdef CS_LITTLE_ENDIAN
+    if (format == RGBA8888)
+    {
+      memcpy (linestart, src, blit_w * sizeof (uint32));
+      src += blit_w * sizeof (uint32);
+    }
+    else
+#endif
+    {
+      for (rx = x; rx < blit_w; rx++)
+      {
+        
+	uint8 r,g,b,a;
+#ifndef CS_LITTLE_ENDIAN
+	if (format == RGBA8888)
+	{
+	  r = *src++;
+	  g = *src++;
+	  b = *src++;
+	  a = *src++;
+	}
+	else
+#endif
+	{
+	  b = *src++;
+	  g = *src++;
+	  r = *src++;
+	  a = *src++;
+	}
+	*linestart++ = r
+		    | (g <<  8)
+		    | (b << 16)
+		    | (a << 24);
+      }
+    }
+  }
+
+  // We don't generate mipmaps or so...
+  flags |= CS_TEXTURE_NOMIPMAPS;
+}
+
+bool csSoftwareTextureHandle::GetRendererDimensions (int &mw, int &mh)
+{
+  PrepareInt ();
+  if (!tex[0]) return false;
+  mw = tex[0]->w;
+  mh = tex[0]->h;
+  return true;
+}
+
+//----------------------------------------------------------------------------//
+
+void csSoftRendererLightmap::DecRef ()
+{
+  if (scfRefCount == 1)							
+  {									
+    CS_ASSERT (slm != 0);
+    slm->FreeRLM (this);
+    return;								
+  }									
+  scfRefCount--;							
+}
+
+csSoftRendererLightmap::csSoftRendererLightmap () : 
+  scfImplementationType (this)
+{
+  dirty = false;
+  data = 0;
+
+  lightCellSize = 0;
+  memset (cacheData, 0, sizeof (cacheData));
+}
+
+csSoftRendererLightmap::~csSoftRendererLightmap ()
+{
+  delete[] data;
+}
+
+void csSoftRendererLightmap::SetSize (size_t lmPixels)
+{
+  delete[] data;
+  lmSize = lmPixels;
+  data = new csRGBpixel[lmSize];
+  lmSize *= sizeof (csRGBpixel);
+}
+
+void csSoftRendererLightmap::GetSLMCoords (int& left, int& top, 
+                                           int& width, int& height)
+{
+  left = rect.xmin; top  = rect.xmin;
+  width = rect.Width (); height = rect.Height ();
+}
+
+void csSoftRendererLightmap::SetData (csRGBpixel* data)
+{
+  memcpy (csSoftRendererLightmap::data, data,
+    lmSize);
+  dirty = true;
+}
+
+void csSoftRendererLightmap::SetLightCellSize (int size)
+{
+  lightCellSize = size;
+  lightCellShift = csLog2 (lightCellSize);
+}
+
+int csSoftRendererLightmap::GetLightCellSize ()
+{
+  return lightCellSize;
+}
+
+//---------------------------------------------------------------------------
+
+csSoftSuperLightmap::csSoftSuperLightmap (csSoftwareTextureManager* texman, 
+					  int width, int height) : 
+  scfImplementationType (this), RLMs(32)
+{
+  w = width;
+  h = height;
+  tex.AttachNew (new csSoftwareTextureHandle (texman, 0, 0));
+}
+
+csSoftSuperLightmap::~csSoftSuperLightmap ()
+{
+}
+
+void csSoftSuperLightmap::FreeRLM (csSoftRendererLightmap* rlm)
+{
+  // IncRef() ourselves manually.
+  // Otherwise freeing the RLM could trigger our own destruction -
+  // causing an assertion in block allocator (due to how BA frees items and
+  // the safety assertions on BA destruction.)
+  scfRefCount++;
+  int id = idmap.GetKey (rlm, ~0);
+  if (id != ~0)
+  {
+    idmap.Delete (id, rlm);
+  }
+  RLMs.Free (rlm);
+  DecRef ();
+}
+
+csSoftRendererLightmap* csSoftSuperLightmap::GetRlmForID (int id)
+{
+  return idmap.Get (id, 0);
+}
+
+csPtr<iRendererLightmap> csSoftSuperLightmap::RegisterLightmap (int left, int top, 
+                                                                int width, int height)
+{
+  csSoftRendererLightmap* rlm = RLMs.Alloc ();
+  rlm->SetSize (width * height);
+  rlm->slm = this;
+  rlm->rect.Set (left, top, left + width, top + height);
+
+  rlm->u1 = left;
+  rlm->v1 = top;
+  rlm->u2 = left + width;
+  rlm->v2 = top  + height;
+
+  const int id = ComputeRlmID (left, top);
+  idmap.Put (id, rlm);
+
+  return csPtr<iRendererLightmap> (rlm);
+}
+
+csPtr<iImage> csSoftSuperLightmap::Dump ()
+{
+  return 0;
+}
+
+iTextureHandle* csSoftSuperLightmap::GetTexture ()
+{
+  return tex;
+}
+
+
+//----------------------------------------------- csSoftwareTextureManager ---//
+
+csSoftwareTextureManager::csSoftwareTextureManager (
+  iObjectRegistry *object_reg,
+  csSoftwareGraphics3DCommon *iG3D, iConfigFile *config)
+  : csTextureManager (object_reg, iG3D->GetDriver2D())
+{
+  read_config (config);
+  G3D = iG3D;
+}
+
+void csSoftwareTextureManager::SetPixelFormat(csPixelFormat const& PixelFormat)
+{
+  pfmt = PixelFormat;
+}
+
+void csSoftwareTextureManager::read_config (iConfigFile *config)
+{
+  csTextureManager::read_config (config);
+  sharpen_mipmaps = config->GetInt
+        ("Video.Software.TextureManager.SharpenMipmaps", 0);
+  debugMipmaps = config->GetBool
+        ("Video.Software.TextureManager.DebugMipmaps", false);
+}
+
+csSoftwareTextureManager::~csSoftwareTextureManager ()
+{
+  Clear ();
+}
+
+void csSoftwareTextureManager::Clear ()
+{
+  csTextureManager::Clear();
+}
+
+int csSoftwareTextureManager::GetTextureFormat ()
+{
+  return CS_IMGFMT_TRUECOLOR | CS_IMGFMT_ALPHA;
+}
+
+uint32 csSoftwareTextureManager::encode_rgb (int r, int g, int b)
+{
+  return
+    ((r >> (8 - pfmt.RedBits))   << pfmt.RedShift) |
+    ((g >> (8 - pfmt.GreenBits)) << pfmt.GreenShift) |
+    ((b >> (8 - pfmt.BlueBits))  << pfmt.BlueShift);
+}
+
+csPtr<iTextureHandle> csSoftwareTextureManager::RegisterTexture (iImage* image,
+  int flags)
+{
+  if (!image)
+  {
+    G3D->Report(CS_REPORTER_SEVERITY_BUG,
+      "BAAAD!!! csSoftwareTextureManager::RegisterTexture with 0 image!");
+
+    csRef<iImage> im (csCreateXORPatternImage(32, 32, 5));
+    image = im;
+    im->IncRef ();	// Avoid smart pointer cleanup. @@@ UGLY
+  }
+
+  csSoftwareTextureHandle *txt = new csSoftwareTextureHandle (
+  	this, image, flags);
+  textures.Push (txt);
+  return csPtr<iTextureHandle> (txt);
+}
+
+void csSoftwareTextureManager::UnregisterTexture (
+		csSoftwareTextureHandle* handle)
+{
+  size_t idx = textures.Find (handle);
+  if (idx != csArrayItemNotFound) textures.DeleteIndexFast (idx);
+}
+
+csPtr<iSuperLightmap> csSoftwareTextureManager::CreateSuperLightmap (
+  int width, int height)
+{
+  return csPtr<iSuperLightmap> (new csSoftSuperLightmap (this, width, height));
+}
+
+void csSoftwareTextureManager::GetMaxTextureSize (int& w, int& h, int& aspect)
+{
+  w = h = 2048;
+  aspect = 32768;
+}
+
+void csSoftwareTextureManager::GetLightmapRendererCoords (
+  int /*slmWidth*/, int /*slmHeight*/, int lm_x1, int lm_y1, int /*lm_x2*/,
+  int /*lm_y2*/, float& lm_u1, float& lm_v1, float &lm_u2, float& lm_v2)
+{
+  lm_u1 = lm_x1;
+  lm_v1 = lm_y1;
+  /*lm_u2 = lm_x2 + 1;
+  lm_v2 = lm_y2 + 1;
+  lm_u1 = lm_x1;
+  lm_v1 =*/ lm_u2 = lm_v2 = 0.0f;
+}
+
+}
+CS_PLUGIN_NAMESPACE_END(Soft3D)

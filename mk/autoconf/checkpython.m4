crystalspace3d/CS

commit 46b090484bd9d60bd9f29c9d2dc0216d039ff77d

res changed the python detection code to separately check whether   the pythonX.X/config dir must be specified to link the python lib.   The reason is that this directory seems to contain a static library   which gets preferred over the shared version, however, this static   lib seems to work somewhat worse than the shared version. Hence,   adjust the lib dir search strategy such that a path with a shared   version is preferred.
git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25656 8cc4aa7f-3514-0410-904f-f2cc9021211c

mk/autoconf/checkpython.m4
@@ -80,10 +80,11 @@ AC_DEFUN([CS_CHECK_PYTHON],
 		print distutils.sysconfig.get_python_lib(0,1)'])
 	    cs_cv_pybase_sysprefix=CS_RUN_PATH_NORMALIZE([$PYTHON -c \
 		'import sys; print sys.prefix'])
-	    cs_cv_pybase_lflags=''
-	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags],
+	    cs_cv_pybase_lflags_base=''
+	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags_base],
 		[cs_cv_pybase_syslib])
-	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags],
+	    cs_cv_pybase_lflags_ext='$cs_cv_pybase_lflags_base'
+	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags_ext],
 		[cs_cv_pybase_syslib], [config])
 	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_sysprefix_lflags],
 		[cs_cv_pybase_sysprefix], [lib])
@@ -122,23 +123,36 @@ AC_DEFUN([CS_CHECK_PYTHON],
 		[], [], CS_EMITTER_OPTIONAL([$1]))
 	    cs_pywinlib=`echo "$cs_cv_pybase" | sed 's/\.//g'`
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
-	       [$cs_cv_pybase_lflags -framework Python])"
+	       [$cs_cv_pybase_lflags_base -framework Python])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
 	       [$cs_cv_pybase_sysprefix_lflags -framework Python])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
-	       [$cs_cv_pybase_lflags -l$cs_cv_pybase])"
+	       [$cs_cv_pybase_lflags_ext -framework Python])"
+	    
+	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
+	       [$cs_cv_pybase_lflags_base -l$cs_cv_pybase])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
 	       [$cs_cv_pybase_sysprefix_lflags -l$cs_cv_pybase])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
-	       [$cs_cv_pybase_lflags -l$cs_pywinlib])"
+	       [$cs_cv_pybase_lflags_ext -l$cs_cv_pybase])"
+	    
+	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
+	       [$cs_cv_pybase_lflags_base -l$cs_pywinlib])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
 	       [$cs_cv_pybase_sysprefix_lflags -l$cs_pywinlib])"
+	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE([],[],
+	       [$cs_cv_pybase_lflags_ext -l$cs_pywinlib])"
+	    
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE(
 	       [],[-bundle -flat_namespace -undefined suppress],
-	       [$cs_cv_pybase_lflags])"
+	       [$cs_cv_pybase_lflags_base])"
 	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE(
 	       [],[-bundle -flat_namespace -undefined suppress],
 	       [$cs_cv_pybase_sysprefix_lflags])"
+	    cs_pyflags="$cs_pyflags CS_CREATE_TUPLE(
+	       [],[-bundle -flat_namespace -undefined suppress],
+	       [$cs_cv_pybase_lflags_ext])"
+	       
 	    CS_CHECK_BUILD([if python SDK is usable], [cs_cv_python],
 		[AC_LANG_PROGRAM([[#include <Python.h>]],
 		    [Py_Initialize(); Py_Finalize();])],

configure
@@ -11096,13 +11096,14 @@ else
   ac_status=$?
   echo "$as_me:$LINENO: \$? = $ac_status" >&5
   (exit $ac_status); } | tr '\\\\' '/' | sed 's/^x//;s/   */ /g;s/^ //;s/ $//'`
-	    cs_cv_pybase_lflags=''
+	    cs_cv_pybase_lflags_base=''
 	    if test -d "$cs_cv_pybase_syslib"; then
-  cs_cv_pybase_lflags="$cs_cv_pybase_lflags -L$cs_cv_pybase_syslib"
+  cs_cv_pybase_lflags_base="$cs_cv_pybase_lflags_base -L$cs_cv_pybase_syslib"
 fi
 
+	    cs_cv_pybase_lflags_ext='$cs_cv_pybase_lflags_base'
 	    if test -d "$cs_cv_pybase_syslib/config"; then
-  cs_cv_pybase_lflags="$cs_cv_pybase_lflags -L$cs_cv_pybase_syslib/config"
+  cs_cv_pybase_lflags_ext="$cs_cv_pybase_lflags_ext -L$cs_cv_pybase_syslib/config"
 fi
 
 	    if test -d "$cs_cv_pybase_sysprefix/lib"; then
@@ -11170,14 +11171,22 @@ echo "${ECHO_T}$cs_cv_python_sdk" >&6
 fi
 
 	    cs_pywinlib=`echo "$cs_cv_pybase" | sed 's/\.//g'`
-	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags -framework Python | sed 'y% %#%'`"
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_base -framework Python | sed 'y% %#%'`"
 	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_sysprefix_lflags -framework Python | sed 'y% %#%'`"
-	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags -l$cs_cv_pybase | sed 'y% %#%'`"
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_ext -framework Python | sed 'y% %#%'`"
+
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_base -l$cs_cv_pybase | sed 'y% %#%'`"
 	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_sysprefix_lflags -l$cs_cv_pybase | sed 'y% %#%'`"
-	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags -l$cs_pywinlib | sed 'y% %#%'`"
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_ext -l$cs_cv_pybase | sed 'y% %#%'`"
+
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_base -l$cs_pywinlib | sed 'y% %#%'`"
 	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_sysprefix_lflags -l$cs_pywinlib | sed 'y% %#%'`"
-	    cs_pyflags="$cs_pyflags `echo @@-bundle -flat_namespace -undefined suppress@$cs_cv_pybase_lflags | sed 'y% %#%'`"
+	    cs_pyflags="$cs_pyflags `echo @@@$cs_cv_pybase_lflags_ext -l$cs_pywinlib | sed 'y% %#%'`"
+
+	    cs_pyflags="$cs_pyflags `echo @@-bundle -flat_namespace -undefined suppress@$cs_cv_pybase_lflags_base | sed 'y% %#%'`"
 	    cs_pyflags="$cs_pyflags `echo @@-bundle -flat_namespace -undefined suppress@$cs_cv_pybase_sysprefix_lflags | sed 'y% %#%'`"
+	    cs_pyflags="$cs_pyflags `echo @@-bundle -flat_namespace -undefined suppress@$cs_cv_pybase_lflags_ext | sed 'y% %#%'`"
+
 	    if test "" != yes; then
   echo "$as_me:$LINENO: checking if python SDK is usable" >&5
 echo $ECHO_N "checking if python SDK is usable... $ECHO_C" >&6

commit 06d9832a2462e3f1044775ab2102ede8ef75da88

Eric Sunshine fixed bug in mk/autoconf/checkpython.m4 introduced by r25656 which resulted in the literal string "$cs_cv_pybase_lflags_base" being used, rather than the interpolation of that shell variable, in build attempts when detecting the Python link library. This should resolve issue #455.

mk/autoconf/checkpython.m4
@@ -87,7 +87,7 @@ AC_DEFUN([CS_CHECK_PYTHON],
 	    cs_cv_pybase_lflags_base=''
 	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags_base],
 		[cs_cv_pybase_syslib])
-	    cs_cv_pybase_lflags_ext='$cs_cv_pybase_lflags_base'
+	    cs_cv_pybase_lflags_ext="$cs_cv_pybase_lflags_base"
 	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_lflags_ext],
 		[cs_cv_pybase_syslib], [config])
 	    _CS_CHECK_PYTHON_LIBDIR([cs_cv_pybase_sysprefix_lflags],
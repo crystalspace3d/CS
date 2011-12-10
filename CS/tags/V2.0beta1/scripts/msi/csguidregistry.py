a3cfa8df7c86fd3f633c451125a41e6b649268eb
philwyett <philwyett@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2011-12-10 19:31:49 +0000

    2.0beta1

---
+++ CS/tags/V2.0beta1/scripts/msi/csguidregistry.py
@@ -0,0 +1,36 @@
+guid_registry = {
+    #We compute some uuids out of a base and a unique id (mostly component guids)
+    "base": '174182C0-BF94-11DC-B42B-0015582877AD',
+
+    #Upgrade codes for our installers. Those may not change.
+    "CS.upgrade_code": '72E45C62-CED0-4149-90A6-3EB3E57D1C7B',
+    "CS.cel.upgrade_code": '9CC654C9-A8FF-4377-884F-47E1A6B26D23',
+    "CS.b2CS.upgrade_code": '74CBDB94-1E1E-4262-8E84-CC5389636C4F',
+    "cel.upgrade_code": '57C3A985-7833-4E46-AE63-E6B32E8BCA7D',
+    "b2cs.upgrade_code": 'C3AB5635-9A53-46F7-AE83-B41BD06C6AD3',
+
+    #CS merge modules
+    "CS.doc.msm": '88888F92-BF93-11DC-B42B-0015582877AD',
+    "CS.vfs.msm": '8DDFD4F0-BF93-11DC-B42B-0015582877AD',
+    "CS.register.msm": '8E4AB45A-BF93-11DC-B42B-0015582877AD',
+    "CS.data.msm": '8EA81938-BF93-11DC-B42B-0015582877AD',
+    "CS.include.msm": '8F1B7996-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.include.msm": '8F6CB040-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.lib.msm": 'A6DDD416-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.executable.msm": 'AC4E1EB0-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.support.msm": 'B18119AA-BF93-11DC-B42B-0015582877AD',
+
+    #cel merge modules
+    "cel.doc.msm": 'BFEEA872-BF93-11DC-B42B-0015582877AD',
+    "cel.vfs.msm": 'C7E67122-BF93-11DC-B42B-0015582877AD',
+    "cel.register.msm": 'D27841CE-BF93-11DC-B42B-0015582877AD',
+    "cel.data.msm": 'DD058534-BF93-11DC-B42B-0015582877AD',
+    "cel.include.msm": 'E639D420-BF93-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.include.msm": 'F65B66AC-BF93-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.lib.msm": '039A60DE-BF94-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.executable.msm": '06DF1D7A-BF94-11DC-B42B-0015582877AD',
+
+    "b2cs.blender.plugin.msm": 'FE7E2FC8-BFF6-11DC-BDF4-001302164650',
+
+    "python": '4E60ED45-1DBB-44AD-A77F-E17E9C8BDCFB',
+    }

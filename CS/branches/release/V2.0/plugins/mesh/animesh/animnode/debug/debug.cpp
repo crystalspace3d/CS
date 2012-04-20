c24882514a3824ea8c70f6fd6d5a1741fd8788f4
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-04-20 13:36:57 +0000

    Merged revisions 37908-37909,37919 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk
    ........
      r37908 | weltall2 | 2012-04-17 12:00:27 +0200 (Tue, 17 Apr 2012) | 2 lines
      
      Don't add the passed filename if not valid (null)
    ........
      r37909 | kickvb | 2012-04-17 15:54:59 +0200 (Tue, 17 Apr 2012) | 2 lines
      
      Fixed wrong Z test
    ........
      r37919 | kickvb | 2012-04-18 14:38:44 +0200 (Wed, 18 Apr 2012) | 2 lines
      
      API doc fix
    ........

--- CS/branches/release/V2.0/plugins/mesh/animesh/animnode/debug/debug.cpp
+++ CS/branches/release/V2.0/plugins/mesh/animesh/animnode/debug/debug.cpp
@@ -210,7 +210,7 @@ CS_PLUGIN_NAMESPACE_BEGIN(DebugNode)
       if (factory->modes & CS::Animation::DEBUG_IMAGES
 	  && factory->image)
       {
-	if (bonePosition < SMALL_Z)
+	if (bonePosition.z < SMALL_Z)
 	  continue;
 
 	csVector4 v1p (projection * csVector4 (bonePosition));
@@ -248,7 +248,7 @@ CS_PLUGIN_NAMESPACE_BEGIN(DebugNode)
 
       if (factory->modes & CS::Animation::DEBUG_SQUARES)
       {
-	if (bonePosition < SMALL_Z)
+	if (bonePosition.z < SMALL_Z)
 	  continue;
 
 	csVector4 v1p (projection * csVector4 (bonePosition));

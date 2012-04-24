9005a3b10ea6bf623e84168094e4e0e7a9715d5b
kickvb <kickvb@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-04-24 14:22:58 +0000

    Project on "Extensions to the cseditor framework"

---
+++ editor/scripts/blender/io_scene_cs/ui/__init__.py
@@ -0,0 +1,11 @@
+__all__ = ["data", "group", "inheritedprops", "object", "settings", "image", "material"]
+
+
+from . import data
+from . import group
+from . import inheritedprops
+from . import object
+from . import settings
+from . import image
+from . import material
+from . import portal

8089a594f09eae31c2eb073b49b91a60299c6290
dominikseifert <dominikseifert@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-05-27 08:03:10 +0000

    - split bullet2.h/cpp into bulletsystem and bulletsector

--- physics/plugins/physics2/bullet/bulletsector.h
+++ physics/plugins/physics2/bullet/bulletsector.h
@@ -1,6 +1,4 @@
 /*
-  Copyright (C) 2011 by Liu Lu
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
   License as published by the Free Software Foundation; either
@@ -15,9 +13,6 @@
   License along with this library; if not, write to the Free
   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-
-#ifndef __CS_BULLET_PHYSICS_H__
-#define __CS_BULLET_PHYSICS_H__
 
 #include "iutil/comp.h"
 #include "csutil/scf.h"
@@ -35,10 +30,8 @@
 
 #include "BulletCollision/CollisionDispatch/btGhostObject.h"
 
-// new csRef: 
-//  CollisionPortal ->  portal, desSector, ghostPortal
-//  csBulletSector ->   sys, hitPortal, (debugDraw, bulletWorld, dispatcher, configuration, solver, broadphase, softWorldInfo)
-//  csBulletSystem ->   
+#ifndef __CS_BULLET_SECTOR_H__
+#define __CS_BULLET_SECTOR_H__
 
 struct iSector;
 class btCollisionObject;
@@ -54,12 +47,6 @@ struct btSoftBodyWorldInfo;
 
 CS_PLUGIN_NAMESPACE_BEGIN (Bullet2)
 {
-/**
- * Determines floating point precision of the bullet2 plugin
- */
-typedef float Real;
-
-
 class csBulletSystem;
 class csBulletSector;
 class csBulletDebugDraw;
@@ -267,94 +254,6 @@ public:
   void AddMovableToSector (CS::Collisions::iCollisionObject* obj);
 
   void RemoveMovableFromSector (CS::Collisions::iCollisionObject* obj);
-};
-
-class csBulletSystem : public scfImplementation3<
-  csBulletSystem, CS::Collisions::iCollisionSystem, 
-  CS::Physics::iPhysicalSystem, iComponent>
-{
-  friend class csBulletColliderConvexMesh;
-  friend class csBulletColliderConcaveMesh;
-  friend class csBulletSector;
-private:
-  iObjectRegistry* object_reg;
-  /*csRefArrayObject<CS::Collisions::iCollider> colliders;
-  csRefArrayObject<csBulletCollisionObject> objects;
-  csRefArrayObject<csBulletRigidBody> rigidBodies;
-  csRefArrayObject<csBulletSoftBody> softBodies;
-  csRefArrayObject<csBulletJoint> joints;
-  csRefArrayObject<iCollisionActor> actors;*/
-  csRefArrayObject<csBulletSector> collSectors;
-  btSoftBodyWorldInfo* defaultInfo;
-  float internalScale;
-  float inverseInternalScale;
-  csStringID baseID;
-  csStringID colldetID;
-
-public:
-  csBulletSystem (iBase* iParent);
-  virtual ~csBulletSystem ();
-
-  // iComponent
-  virtual bool Initialize (iObjectRegistry* object_reg);
-
-  // iCollisionSystem
-  virtual void SetInternalScale (float scale);
-  virtual csRef<CS::Collisions::iColliderConvexMesh> CreateColliderConvexMesh (
-    iMeshWrapper* mesh, bool simplify = false);
-  virtual csRef<CS::Collisions::iColliderConcaveMesh> CreateColliderConcaveMesh (iMeshWrapper* mesh);
-  virtual csRef<CS::Collisions::iColliderConcaveMeshScaled> CreateColliderConcaveMeshScaled
-      (CS::Collisions::iColliderConcaveMesh* collider, csVector3 scale);
-  virtual csRef<CS::Collisions::iColliderCylinder> CreateColliderCylinder (float length, float radius);
-  virtual csRef<CS::Collisions::iColliderBox> CreateColliderBox (const csVector3& size);
-  virtual csRef<CS::Collisions::iColliderSphere> CreateColliderSphere (float radius);
-  virtual csRef<CS::Collisions::iColliderCapsule> CreateColliderCapsule (float length, float radius);
-  virtual csRef<CS::Collisions::iColliderCone> CreateColliderCone (float length, float radius);
-  virtual csRef<CS::Collisions::iColliderPlane> CreateColliderPlane (const csPlane3& plane);
-  virtual csRef<CS::Collisions::iColliderTerrain> CreateColliderTerrain (iTerrainSystem* terrain,
-      float minHeight = 0, float maxHeight = 0);
-
-  virtual csRef<CS::Collisions::iCollisionObject> CreateCollisionObject ();
-  virtual csRef<CS::Collisions::iCollisionActor> CreateCollisionActor ();
-  virtual csRef<CS::Collisions::iCollisionSector> CreateCollisionSector ();
-  virtual CS::Collisions::iCollisionSector* FindCollisionSector (const char* name);
-
-  virtual void DecomposeConcaveMesh (CS::Collisions::iCollisionObject* object,
-    iMeshWrapper* mesh, bool simplify = false); 
-
-  //iPhysicalSystem
-  virtual csRef<CS::Physics::iRigidBody> CreateRigidBody ();
-
-  virtual csRef<CS::Physics::iJoint> CreateJoint ();
-  virtual csRef<CS::Physics::iJoint> CreateRigidP2PJoint (const csVector3 position);
-  virtual csRef<CS::Physics::iJoint> CreateRigidSlideJoint (const csOrthoTransform trans,
-    float minDist, float maxDist, float minAngle, float maxAngle, int axis);
-  virtual csRef<CS::Physics::iJoint> CreateRigidHingeJoint (const csVector3 position,
-    float minAngle, float maxAngle, int axis);
-  virtual csRef<CS::Physics::iJoint> CreateRigidConeTwistJoint (const csOrthoTransform trans,
-    float swingSpan1,float swingSpan2,float twistSpan);
-  virtual csRef<CS::Physics::iJoint> CreateSoftLinearJoint (const csVector3 position);
-  virtual csRef<CS::Physics::iJoint> CreateSoftAngularJoint (int axis);
-  virtual csRef<CS::Physics::iJoint> CreateRigidPivotJoint (CS::Physics::iRigidBody* body, const csVector3 position);
- 
-  virtual csRef<CS::Physics::iSoftBody> CreateRope (csVector3 start,
-      csVector3 end, size_t segmentCount);
-  virtual csRef<CS::Physics::iSoftBody> CreateRope (csVector3* vertices, size_t vertexCount);
-  virtual csRef<CS::Physics::iSoftBody> CreateCloth (csVector3 corner1, csVector3 corner2,
-      csVector3 corner3, csVector3 corner4,
-      size_t segmentCount1, size_t segmentCount2,
-      bool withDiagonals = false);
-
-  virtual csRef<CS::Physics::iSoftBody> CreateSoftBody (iGeneralFactoryState* genmeshFactory, 
-    const csOrthoTransform& bodyTransform);
-
-  virtual csRef<CS::Physics::iSoftBody> CreateSoftBody (csVector3* vertices,
-      size_t vertexCount, csTriangle* triangles, size_t triangleCount,
-      const csOrthoTransform& bodyTransform);
-  float getInverseInternalScale() {return inverseInternalScale;}
-  float getInternalScale() {return internalScale;}
-
-  void ReportWarning (const char* msg, ...);
 };
 }
 CS_PLUGIN_NAMESPACE_END(Bullet2)

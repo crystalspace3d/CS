39545e804a3636c8d193e0a8a837c476dd086382
crystal-manage <crystal-manage@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-07-02 02:07:58 +0000

    master header files: Automated master header file repair. Visual-C++ project files: Automated Visual-C++ project file repair. Texinfo files: Automated Texinfo @node and @menu repair. Swig Python files: Automated Swig Python file repair. Swig Perl5 files: Automated Swig Perl5 file repair. User's Manual: Automated Texinfo to HTML conversion. csver.h: Automated csver.h regeneration.

--- V2.0/include/csver.h
+++ V2.0/include/csver.h
@@ -2,17 +2,17 @@
    make changes _there_ instead */
 /*
     Copyright (C) 1998-2007 by Jorrit Tyberghein
-  
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-  
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-  
+
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -57,7 +57,7 @@
  */
 //CS_RCSREV_OFFSET 0
 #ifndef CS_VERSION_RCSREV
-#define CS_VERSION_RCSREV	2203
+#define CS_VERSION_RCSREV	0
 #endif
 
 /**\name Version number definitions (numeric)
@@ -95,7 +95,7 @@
 /// Build number
 #define CS_VERSION_BUILD	CS_VER_QUOTE(CS_VERSION_NUM_BUILD)
 /// Date of release
-#define CS_RELEASE_DATE		CS_VER_QUOTE_(Tue 5-Apr-2011)
+#define CS_RELEASE_DATE		CS_VER_QUOTE_(Tue 03-July-2012)
 /** @} */
 
 #define CS_VER_QUOTE_(X) #X

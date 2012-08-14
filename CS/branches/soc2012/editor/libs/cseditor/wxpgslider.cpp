5cb3630b6b66ec364e32ab217cd033e631af5126
andreibarsan <andreibarsan@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2012-08-14 10:08:07 +0000

    - visually improved the slider

--- CS/branches/soc2012/editor/libs/cseditor/wxpgslider.cpp
+++ CS/branches/soc2012/editor/libs/cseditor/wxpgslider.cpp
@@ -10,7 +10,7 @@ wxPGWindowList wxPGSliderEditor::CreateControls( wxPropertyGrid*  propgrid,
                                                  const wxSize&    size ) const
 {
   wxSlider* slider = ((wxPGSliderProperty*) property)->GetSlider ();
-  ((wxWindow*) slider)->Hide ();
+  //((wxWindow*) slider)->Hide ();
 
   double v_d = property->GetValue().GetDouble();
   if ( v_d < 0 )
@@ -18,6 +18,8 @@ wxPGWindowList wxPGSliderEditor::CreateControls( wxPropertyGrid*  propgrid,
   else if ( v_d > 1 )
     v_d = 1;
 
+  // TODO: find why it's being re-created
+  /*
   wxSlider* ctrl = new wxSlider ();
   ctrl->Create ( propgrid->GetPanel(),
                  wxPG_SUBID1,
@@ -27,13 +29,15 @@ wxPGWindowList wxPGSliderEditor::CreateControls( wxPropertyGrid*  propgrid,
                  pos,
                  size,
                  wxSL_HORIZONTAL );
+                 //*/
 
   // Setup event handler
   propgrid->Connect ( wxPG_SUBID1, wxEVT_SCROLL_THUMBTRACK, 
                       (wxObjectEventFunction) (wxEventFunction) (wxCommandEventFunction)
                       &wxPropertyGrid::OnCustomEditorEvent, NULL, propgrid );
 
-  return ctrl;
+  //return ctrl;
+   return slider;
 }
 
 void wxPGSliderEditor::UpdateControl ( wxPGProperty* property, wxWindow* wnd ) const
@@ -50,7 +54,8 @@ void wxPGSliderEditor::UpdateControl ( wxPGProperty* property, wxWindow* wnd ) c
         val = 1;
       ctrl->SetValue ( (int)(val * precision) );
 
-      // textVal->SetValue(wxString::Format(wxT("%ld"), val * precision));
+      static_cast<wxPGSliderProperty*>(property)->GetLabel()
+        ->SetValue( wxString::Format(wxT("%ld"), val * precision) );
     }
   }
 }
@@ -95,13 +100,19 @@ void wxPGSliderEditor::DrawValue ( wxDC&           dc,
   int curr = (int) ((double) property->GetValue() * precision);
   slider-> SetValue (curr);
 
+  wxPanel* pan = static_cast<wxPGSliderProperty*>(property)->GetPanel();
+  if( !pan->IsShown()) {
+    printf("Panel wasn't shown... Odd...\n");
+    pan->Show();
+  }
+
   wxWindow* win = (wxWindow*) slider;
- // if (!win-> IsShown ())
-  //  win-> Show ();
+  if (!win-> IsShown ())
+    win-> Show ();
 
   wxTextCtrl* textCtrl = ( static_cast<wxPGSliderProperty*>(property) )->GetLabel();
- // if (!textCtrl->IsShown() )
-  //  textCtrl->Show();
+  if (!textCtrl->IsShown() )
+    textCtrl->Show();
 }
 
 //----------------- wxPGSliderProperty ---------------------
@@ -123,6 +134,11 @@ wxPGSliderProperty::wxPGSliderProperty ( const wxString&  label,
 
   // Calls DoRegisterEditorClass; mandatory for custom properties
   wxPGEditor* editorPointer = wxPropertyGrid::RegisterEditorClass( new wxPGSliderEditor (max), wxT("SliderEditor") );
+  /*
+  wxPropertyGrid* grid = GetGrid();
+  wxRect rect = GetRect(grid);
+  editorPointer->CreateControls(grid, this, wxPoint(rect.x, rect.y), rect.GetSize());
+  */
   SetEditor ( editorPointer );
 }
 
@@ -131,28 +147,41 @@ wxPGSliderProperty::~wxPGSliderProperty ()
 
 void wxPGSliderProperty::Init ()
 {
-  // TODO: throw them both in a sizer
-  ctrl = new wxSlider();
-  double v_d = GetValue().GetDouble();
   wxPropertyGrid* propgrid = GetGrid();
   wxRect rect = GetRect(propgrid);
-  ctrl->Create( propgrid->GetPanel(),
+
+  wrapper = new wxPanel(propgrid->GetPanel(), 
+                        rect.x,
+                        rect.y,
+                        rect.width,
+                        rect.height
+                        );
+
+  wrapperSizer = new wxBoxSizer(wxHORIZONTAL);
+  wrapperSizer->SetDimension(0, 0, rect.width, rect.height);
+  wrapper->SetSizer(wrapperSizer);
+  
+  ctrl = new wxSlider();
+  double v_d = GetValue().GetDouble();
+  ctrl->Create( wrapper,
                 wxPG_SUBID1,
                 (int)(v_d * maxVal),
                 minVal,
                 maxVal,
-                rect.GetPosition(),
-                rect.GetSize(),
-                wxSL_HORIZONTAL);
+                wxPoint(rect.width / 2 + 1, -1),
+                wxDefaultSize
+                );
+  wrapperSizer->Add(ctrl, 1, wxALL, 4);
 
   textCtrl = new wxTextCtrl();
-  textCtrl->Create( propgrid->GetPanel(),
+  textCtrl->Create( wrapper,
                     wxPG_SUBID1,
                     wxString::Format(wxT("%ld"), v_d),
-                    wxPoint(rect.GetPosition().x - labelWidth, rect.GetPosition().y),
-                    wxSize(labelWidth, -1),
-                    wxSL_HORIZONTAL);
+                    wxDefaultPosition,
+                    wxDefaultSize);
+  wrapperSizer->Add(textCtrl, 1, wxALL, 4);
 
+  GetEditorClass()->CreateControls(propgrid, this, rect.GetPosition(), rect.GetSize());
   // Read-only for now
   // Afterwards, setup event handlers so that each edit (slider, text) triggers
   // the update of the other representation.
@@ -169,13 +198,18 @@ wxTextCtrl* wxPGSliderProperty::GetLabel () const
   return textCtrl;
 }
 
+wxPanel* wxPGSliderProperty::GetPanel() const 
+{
+  return wrapper;
+}
+
 wxRect wxPGSliderProperty::GetRect (wxPropertyGrid* propgrid) const
 {
   wxRect rect = propgrid->GetPropertyRect(  dynamic_cast<const wxPGProperty*>(this), 
                                             dynamic_cast<const wxPGProperty*>(this) );
   wxSize size = rect.GetSize();
   wxPoint pos = rect.GetPosition();
-  int splitterPos = propgrid->GetSplitterPosition() + labelWidth + 2;
+  int splitterPos = propgrid->GetSplitterPosition() + 2;
   pos.x += splitterPos;
   size.x -= splitterPos;
   size.y -= 1;
@@ -185,9 +219,14 @@ wxRect wxPGSliderProperty::GetRect (wxPropertyGrid* propgrid) const
 
 wxString wxPGSliderProperty::GetValueAsString ( int argflags ) const
 {
-  return wxString("", wxConvUTF8);
+  return wxString::Format(wxT("%ld"), GetSlider()->GetValue() * maxVal);
 }
 
 wxVariant wxPGSliderProperty::GetValue() const {
   return wxVariant(GetSlider()->GetValue() * maxVal);
+}
+
+void wxPGSliderProperty::OnScroll(wxEvent& event)
+{
+
 }

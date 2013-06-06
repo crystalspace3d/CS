02429c53dde18e680fdd46da57d28de71d7bdfaa
res2002 <res2002@0c7a39be-3cd5-40b7-bf08-85d6bfde069a>
2013-06-06 11:18:49 +0000

    SoC 2013: Water simulation

---
+++ water/plugins/physics2/bullet/common2.cpp
@@ -0,0 +1,31 @@
+/*
+    Copyright (C) 2011-2012 Christian Van Brussel, Institute of Information
+      and Communication Technologies, Electronics and Applied Mathematics
+      at Universite catholique de Louvain, Belgium
+      http://www.uclouvain.be/en-icteam.html
+    Copyright (C) 2012 by Dominik Seifert
+    Copyright (C) 2011 by Liu Lu
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "common2.h"
+
+CS_PLUGIN_NAMESPACE_BEGIN(Bullet2)
+{
+
+}
+CS_PLUGIN_NAMESPACE_END(Bullet2)
